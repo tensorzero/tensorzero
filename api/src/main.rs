c224a1f2edbac4ad00ab77542bbce44fea18ebe5
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2024-07-16 18:12:16 -0400

    Add status handler

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,11 +2,15 @@
 
 use axum::response::IntoResponse;
 use axum::routing::get;
-use axum::Router;
+use axum::{debug_handler, Router};
+
+mod status;
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/status", get(status::status_handler));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -17,6 +21,7 @@ async fn main() {
         .expect("Failed to start server")
 }
 
+#[debug_handler]
 async fn hello_world() -> impl IntoResponse {
     "HELL0 W0RLD"
 }
