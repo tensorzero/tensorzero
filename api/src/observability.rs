c38f458950ff72b474ffcd136456ce4d415bf8f7
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2024-07-29 17:41:38 -0400

    Add logs

--- api/src/observability.rs
+++ api/src/observability.rs
@@ -1,9 +1,29 @@
 use metrics_exporter_prometheus::PrometheusBuilder;
 use std::net::SocketAddr;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 /// Set up observability
 pub fn setup() {
+    setup_logs();
     setup_metrics();
+}
+
+/// Set up logs
+fn setup_logs() {
+    // Get the current log level from the environment variable `RUST_LOG`
+    let log_level = tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| "api=debug,warn".into());
+
+    tracing_subscriber::registry()
+        .with(log_level)
+        .with(
+            tracing_subscriber::fmt::layer()
+                .json()
+                .flatten_event(true)
+                .with_current_span(false)
+                .with_target(false),
+        )
+        .init();
 }
 
 /// Set up Prometheus metrics exporter
