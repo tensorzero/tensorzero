aaaa805d45de042751611983e3549822d7c850a9
Viraj Mehta <viraj@tensorzero.com>
2024-08-15 15:18:29 -0400

    json e2e tests in

--- gateway/tests/e2e/together.rs
+++ gateway/tests/e2e/together.rs
@@ -19,7 +19,6 @@ use crate::e2e::common::{
 ///
 /// TODOs (#81):
 ///  - tool calling
-///  - JSON mode
 ///  - other API parameters (temp, max_tokens, etc.)
 
 #[tokio::test]
@@ -427,3 +426,121 @@ async fn test_streaming() {
 //     assert!(result.get("ttft_ms").unwrap().is_null());
 //     result.get("raw_response").unwrap().as_str().unwrap();
 // }
+
+#[tokio::test]
+async fn test_json_request() {
+    let episode_id = Uuid::now_v7();
+
+    let payload = json!({
+        "function_name": "json_succeed",
+        "episode_id": episode_id,
+        "input":
+            {
+                "system": {"assistant_name": "AskJeeves"},
+                "messages": [
+                {
+                    "role": "user",
+                    "content": "What's the capital of Texas?"
+                }
+            ]},
+        "stream": false,
+        "variant_name": "together"
+    });
+
+    let response = Client::new()
+        .post(get_gateway_endpoint("/inference"))
+        .json(&payload)
+        .send()
+        .await
+        .unwrap();
+    // Check Response is OK, then fields in order
+    assert_eq!(response.status(), StatusCode::OK);
+    let response_json = response.json::<Value>().await.unwrap();
+    let output = response_json.get("output").unwrap().as_object().unwrap();
+    println!("{:?}", output);
+    let parsed = output.get("parsed").unwrap().as_object().unwrap();
+    parsed.get("answer").unwrap().as_str().unwrap();
+    output.get("raw").unwrap().as_str().unwrap();
+    // Check that created is here
+    response_json.get("created").unwrap();
+    // Check that inference_id is here
+    let inference_id = response_json.get("inference_id").unwrap().as_str().unwrap();
+    let inference_id = Uuid::parse_str(inference_id).unwrap();
+    // Check that type is "json"
+    let r#type = response_json.get("type").unwrap().as_str().unwrap();
+    assert_eq!(r#type, "json");
+
+    // Check that usage is correct
+    let usage = response_json.get("usage").unwrap();
+    let usage = usage.as_object().unwrap();
+    let prompt_tokens = usage.get("prompt_tokens").unwrap().as_u64().unwrap();
+    let completion_tokens = usage.get("completion_tokens").unwrap().as_u64().unwrap();
+    assert!(prompt_tokens > 10);
+    assert!(completion_tokens > 5);
+    // Sleep for 1 second to allow time for data to be inserted into ClickHouse (trailing writes from API)
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+    // Check ClickHouse
+    let clickhouse = get_clickhouse().await;
+    let result = select_inference_clickhouse(&clickhouse, inference_id)
+        .await
+        .unwrap();
+    let id = result.get("id").unwrap().as_str().unwrap();
+    let id_uuid = Uuid::parse_str(id).unwrap();
+    assert_eq!(id_uuid, inference_id);
+    let correct_input = json!({
+        "system": {"assistant_name": "AskJeeves"},
+        "messages": [
+            {
+                "role": "user",
+                "content": [{"type": "text", "value": "What's the capital of Texas?"}]
+            }
+        ]
+    });
+    let input: Value =
+        serde_json::from_str(result.get("input").unwrap().as_str().unwrap()).unwrap();
+    assert_eq!(input, correct_input);
+    // Check that correctly parsed output is present
+    let output = result.get("output").unwrap().as_str().unwrap();
+    let output: Value = serde_json::from_str(output).unwrap();
+    let parsed = output.get("parsed").unwrap().as_object().unwrap();
+    parsed.get("answer").unwrap().as_str().unwrap();
+    output.get("raw").unwrap().as_str().unwrap();
+    // Check content blocks
+    let retrieved_episode_id = result.get("episode_id").unwrap().as_str().unwrap();
+    let retrieved_episode_id = Uuid::parse_str(retrieved_episode_id).unwrap();
+    assert_eq!(retrieved_episode_id, episode_id);
+    // Check the variant name
+    let variant_name = result.get("variant_name").unwrap().as_str().unwrap();
+    assert_eq!(variant_name, "together");
+
+    // Check the ModelInference Table
+    let result = select_model_inferences_clickhouse(&clickhouse, inference_id)
+        .await
+        .unwrap();
+    let id = result.get("id").unwrap().as_str().unwrap();
+    let _ = Uuid::parse_str(id).unwrap();
+    let inference_id_result = result.get("inference_id").unwrap().as_str().unwrap();
+    let inference_id_result = Uuid::parse_str(inference_id_result).unwrap();
+    assert_eq!(inference_id_result, inference_id);
+
+    let input_result = result.get("input").unwrap().as_str().unwrap();
+    let input_result: Value = serde_json::from_str(input_result).unwrap();
+    assert_eq!(input_result, correct_input);
+    let output = result.get("output").unwrap().as_str().unwrap();
+    let content_blocks: Vec<Value> = serde_json::from_str(output).unwrap();
+    assert_eq!(content_blocks.len(), 1);
+    let content_block = content_blocks.first().unwrap();
+    let content_block_type = content_block.get("type").unwrap().as_str().unwrap();
+    assert_eq!(content_block_type, "text");
+    content_block.get("text").unwrap().as_str().unwrap();
+
+    let input_tokens = result.get("input_tokens").unwrap().as_u64().unwrap();
+    assert!(input_tokens > 10);
+    let output_tokens = result.get("output_tokens").unwrap().as_u64().unwrap();
+    assert!(output_tokens > 5);
+    let response_time_ms = result.get("response_time_ms").unwrap().as_u64().unwrap();
+    assert!(response_time_ms > 0);
+    assert!(result.get("ttft_ms").unwrap().is_null());
+    result.get("raw_response").unwrap().as_str().unwrap();
+}
