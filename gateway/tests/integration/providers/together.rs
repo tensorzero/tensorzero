e6abae6dfe364ecd4a85ebd632bed3cf0c0eb36e
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2024-08-20 19:15:25 -0400

    Update

--- gateway/tests/integration/providers/together.rs
+++ gateway/tests/integration/providers/together.rs
@@ -9,15 +9,44 @@ use crate::providers::common::TestProviders;
 crate::generate_provider_tests!(get_providers);
 
 async fn get_providers() -> TestProviders {
+    let provider = Box::leak(Box::new(get_provider()));
+    let provider_tool_use = Box::leak(Box::new(get_provider_tool_use()));
+
+    TestProviders {
+        simple_inference: vec![provider],
+        streaming_inference: vec![provider],
+        tool_use_inference: vec![provider_tool_use],
+        tool_use_streaming_inference: vec![provider_tool_use],
+        tool_result_inference: vec![provider_tool_use],
+        tool_result_streaming_inference: vec![provider_tool_use],
+        json_mode_inference: vec![provider],
+        json_mode_streaming_inference: vec![provider],
+    }
+}
+
+/// Get a generic provider for testing
+fn get_provider() -> ProviderConfig {
     // Get a generic provider for testing
     let api_key = env::var("TOGETHER_API_KEY").expect("TOGETHER_API_KEY must be set");
     let api_key = Some(SecretString::new(api_key));
     let model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo".to_string();
 
-    let provider = ProviderConfig::Together(TogetherProvider {
+    ProviderConfig::Together(TogetherProvider {
         model_name,
         api_key,
-    });
+    })
+}
 
-    TestProviders::with_provider(provider)
+// Get a generic provider for tool use
+// TODO (#80): Together has a different function calling format for Llama 3.
+fn get_provider_tool_use() -> ProviderConfig {
+    // Get a generic provider for testing
+    let api_key = env::var("TOGETHER_API_KEY").expect("TOGETHER_API_KEY must be set");
+    let api_key = Some(SecretString::new(api_key));
+    let model_name = "mistralai/Mistral-7B-Instruct-v0.1".to_string();
+
+    ProviderConfig::Together(TogetherProvider {
+        model_name,
+        api_key,
+    })
 }
