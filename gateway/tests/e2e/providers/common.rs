a53835245ee2609dadea8f5bb647232864d53cf8
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2024-08-21 15:53:12 -0400

    Update

--- gateway/tests/e2e/providers/common.rs
+++ gateway/tests/e2e/providers/common.rs
@@ -25,7 +25,7 @@ pub struct E2ETestProviders {
     pub simple_inference: Vec<E2ETestProvider>,
     pub streaming_inference: Vec<E2ETestProvider>,
     pub tool_use_inference: Vec<E2ETestProvider>,
-    // pub tool_use_streaming_inference: Vec<E2ETestProvider>,
+    pub tool_use_streaming_inference: Vec<E2ETestProvider>,
     // pub tool_result_inference: Vec<E2ETestProvider>,
     // pub tool_result_streaming_inference: Vec<E2ETestProvider>,
     // pub json_mode_inference: Vec<E2ETestProvider>,
@@ -40,7 +40,7 @@ impl E2ETestProviders {
             simple_inference: providers.clone(),
             streaming_inference: providers.clone(),
             tool_use_inference: providers.clone(),
-            // tool_use_streaming_inference: providers.clone(),
+            tool_use_streaming_inference: providers.clone(),
             // tool_result_inference: providers.clone(),
             // tool_result_streaming_inference: providers.clone(),
             // json_mode_inference: providers.clone(),
@@ -53,7 +53,7 @@ impl E2ETestProviders {
             simple_inference: providers.clone(),
             streaming_inference: providers.clone(),
             tool_use_inference: providers.clone(),
-            // tool_use_streaming_inference: providers.clone(),
+            tool_use_streaming_inference: providers.clone(),
             // tool_result_inference: static_providers.clone(),
             // tool_result_streaming_inference: providers.clone(),
             // json_mode_inference: providers.clone(),
@@ -72,7 +72,7 @@ macro_rules! generate_provider_tests {
         // use $crate::providers::common::test_tool_result_inference_request_with_provider;
         // use $crate::providers::common::test_tool_result_streaming_inference_request_with_provider;
         use $crate::providers::common::test_tool_use_inference_request_with_provider;
-        // use $crate::providers::common::test_tool_use_streaming_inference_request_with_provider;
+        use $crate::providers::common::test_tool_use_streaming_inference_request_with_provider;
 
         #[tokio::test]
         async fn test_simple_inference_request() {
@@ -98,13 +98,13 @@ macro_rules! generate_provider_tests {
             }
         }
 
-        // #[tokio::test]
-        // async fn test_tool_use_streaming_inference_request() {
-        //     let providers = $func().await.tool_use_streaming_inference;
-        //     for provider in providers {
-        //         test_tool_use_streaming_inference_request_with_provider(provider).await;
-        //     }
-        // }
+        #[tokio::test]
+        async fn test_tool_use_streaming_inference_request() {
+            let providers = $func().await.tool_use_streaming_inference;
+            for provider in providers {
+                test_tool_use_streaming_inference_request_with_provider(provider).await;
+            }
+        }
 
         // #[tokio::test]
         // async fn test_json_mode_inference_request() {
@@ -384,6 +384,9 @@ pub async fn test_streaming_inference_request_with_provider(provider: E2ETestPro
     if provider.variant_name != "azure" {
         assert!(prompt_tokens > 5);
         assert!(completion_tokens > 5);
+    } else {
+        assert_eq!(prompt_tokens, 0);
+        assert_eq!(completion_tokens, 0);
     }
 
     // Sleep to allow time for data to be inserted into ClickHouse (trailing writes from API)
@@ -477,18 +480,22 @@ pub async fn test_streaming_inference_request_with_provider(provider: E2ETestPro
     assert_eq!(clickhouse_content, full_content);
 
     let raw_response = result.get("raw_response").unwrap().as_str().unwrap();
-    assert!(raw_response.contains("Tokyo"));
+
     // Check if raw_response is valid JSONL
     for line in raw_response.lines() {
         assert!(serde_json::from_str::<Value>(line).is_ok());
     }
 
+    let input_tokens = result.get("input_tokens").unwrap().as_u64().unwrap();
+    let output_tokens = result.get("output_tokens").unwrap().as_u64().unwrap();
+
     // NB: Azure doesn't support input/output tokens during streaming
     if provider.variant_name != "azure" {
-        let input_tokens = result.get("input_tokens").unwrap().as_u64().unwrap();
         assert!(input_tokens > 5);
-        let output_tokens = result.get("output_tokens").unwrap().as_u64().unwrap();
         assert!(output_tokens > 5);
+    } else {
+        assert_eq!(input_tokens, 0);
+        assert_eq!(output_tokens, 0);
     }
 
     let response_time_ms = result.get("response_time_ms").unwrap().as_u64().unwrap();
@@ -510,7 +517,7 @@ pub async fn test_tool_use_inference_request_with_provider(provider: E2ETestProv
             "messages": [
                 {
                     "role": "user",
-                    "content": "What is the weather like in Tokyo?"
+                    "content": "What is the weather like in Tokyo? Use the `get_weather` tool."
                 }
             ]},
         "stream": false,
@@ -577,7 +584,7 @@ pub async fn test_tool_use_inference_request_with_provider(provider: E2ETestProv
     let usage = usage.as_object().unwrap();
     let prompt_tokens = usage.get("prompt_tokens").unwrap().as_u64().unwrap();
     let completion_tokens = usage.get("completion_tokens").unwrap().as_u64().unwrap();
-    assert!(prompt_tokens > 10);
+    assert!(prompt_tokens > 0);
     assert!(completion_tokens > 0);
 
     // Sleep to allow time for data to be inserted into ClickHouse (trailing writes from API)
@@ -613,7 +620,7 @@ pub async fn test_tool_use_inference_request_with_provider(provider: E2ETestProv
             "messages": [
                 {
                     "role": "user",
-                    "content": [{"type": "text", "value": "What is the weather like in Tokyo?"}]
+                    "content": [{"type": "text", "value": "What is the weather like in Tokyo? Use the `get_weather` tool."}]
                 }
             ]
         }
@@ -707,10 +714,268 @@ pub async fn test_tool_use_inference_request_with_provider(provider: E2ETestProv
     assert!(raw_response.contains("get_weather"));
 
     let input_tokens = result.get("input_tokens").unwrap().as_u64().unwrap();
-    assert!(input_tokens > 10);
+    assert!(input_tokens > 0);
     let output_tokens = result.get("output_tokens").unwrap().as_u64().unwrap();
     assert!(output_tokens > 0);
     let response_time_ms = result.get("response_time_ms").unwrap().as_u64().unwrap();
     assert!(response_time_ms > 0);
     assert!(result.get("ttft_ms").unwrap().is_null());
+}
+
+pub async fn test_tool_use_streaming_inference_request_with_provider(provider: E2ETestProvider) {
+    let episode_id = Uuid::now_v7();
+
+    let payload = json!({
+        "function_name": "weather_helper",
+        "episode_id": episode_id,
+        "input":{
+            "system": {"assistant_name": "Professor Aqua"},
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "What is the weather like in Tokyo? Use the `get_weather` tool."
+                }
+            ]},
+        "stream": true,
+        "variant_name": provider.variant_name,
+    });
+
+    let mut event_source = Client::new()
+        .post(get_gateway_endpoint("/inference"))
+        .json(&payload)
+        .eventsource()
+        .unwrap();
+
+    let mut chunks = vec![];
+    let mut found_done_chunk = false;
+    while let Some(event) = event_source.next().await {
+        let event = event.unwrap();
+        match event {
+            Event::Open => continue,
+            Event::Message(message) => {
+                if message.data == "[DONE]" {
+                    found_done_chunk = true;
+                    break;
+                }
+                chunks.push(message.data);
+            }
+        }
+    }
+    assert!(found_done_chunk);
+
+    let mut inference_id = None;
+    let mut tool_id: Option<String> = None;
+    let mut arguments = String::new();
+    let mut prompt_tokens = 0;
+    let mut completion_tokens = 0;
+
+    for chunk in chunks {
+        let chunk_json: Value = serde_json::from_str(&chunk).unwrap();
+
+        let chunk_inference_id = chunk_json.get("inference_id").unwrap().as_str().unwrap();
+        let chunk_inference_id = Uuid::parse_str(chunk_inference_id).unwrap();
+        match inference_id {
+            None => inference_id = Some(chunk_inference_id),
+            Some(inference_id) => assert_eq!(inference_id, chunk_inference_id),
+        }
+
+        let chunk_episode_id = chunk_json.get("episode_id").unwrap().as_str().unwrap();
+        let chunk_episode_id = Uuid::parse_str(chunk_episode_id).unwrap();
+        assert_eq!(chunk_episode_id, episode_id);
+
+        for block in chunk_json.get("content").unwrap().as_array().unwrap() {
+            let block_type = block.get("type").unwrap().as_str().unwrap();
+            assert_eq!(block_type, "tool_call");
+
+            assert!(block.get("id").is_some());
+            assert_eq!(block.get("name").unwrap().as_str().unwrap(), "get_weather");
+
+            let block_tool_id = block.get("id").unwrap().as_str().unwrap();
+            match &tool_id {
+                None => tool_id = Some(block_tool_id.to_string()),
+                Some(tool_id) => assert_eq!(tool_id, block_tool_id),
+            }
+
+            let chunk_arguments = block.get("arguments").unwrap().as_str().unwrap();
+            arguments.push_str(chunk_arguments);
+        }
+
+        if let Some(usage) = chunk_json.get("usage").and_then(|u| u.as_object()) {
+            prompt_tokens += usage.get("prompt_tokens").unwrap().as_u64().unwrap();
+            completion_tokens += usage.get("completion_tokens").unwrap().as_u64().unwrap();
+        }
+    }
+
+    // NB: Azure doesn't return usage during streaming
+    if provider.variant_name != "azure" {
+        assert!(prompt_tokens > 0);
+        assert!(completion_tokens > 0);
+    } else {
+        assert_eq!(prompt_tokens, 0);
+        assert_eq!(completion_tokens, 0);
+    }
+
+    let inference_id = inference_id.unwrap();
+    let tool_id = tool_id.unwrap();
+    assert!(serde_json::from_str::<Value>(&arguments).is_ok());
+
+    // Sleep for 1 second to allow time for data to be inserted into ClickHouse (trailing writes from API)
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+    // Check ClickHouse - Inference Table
+    let clickhouse = get_clickhouse().await;
+    let result = select_inference_clickhouse(&clickhouse, inference_id)
+        .await
+        .unwrap();
+
+    let id = result.get("id").unwrap().as_str().unwrap();
+    let id_uuid = Uuid::parse_str(id).unwrap();
+    assert_eq!(id_uuid, inference_id);
+
+    let function_name = result.get("function_name").unwrap().as_str().unwrap();
+    assert_eq!(function_name, "weather_helper");
+
+    let variant_name = result.get("variant_name").unwrap().as_str().unwrap();
+    assert_eq!(variant_name, provider.variant_name);
+
+    let episode_id_result = result.get("episode_id").unwrap().as_str().unwrap();
+    let episode_id_result = Uuid::parse_str(episode_id_result).unwrap();
+    assert_eq!(episode_id_result, episode_id);
+
+    let input: Value =
+        serde_json::from_str(result.get("input").unwrap().as_str().unwrap()).unwrap();
+    let correct_input: Value = json!(
+        {
+            "system": {
+                "assistant_name": "Professor Aqua"
+            },
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [{"type": "text", "value": "What is the weather like in Tokyo? Use the `get_weather` tool."}]
+                }
+            ]
+        }
+    );
+    assert_eq!(input, correct_input);
+
+    let output_clickhouse: Vec<Value> =
+        serde_json::from_str(result.get("output").unwrap().as_str().unwrap()).unwrap();
+    assert_eq!(output_clickhouse.len(), 1);
+    let content_block = output_clickhouse.first().unwrap();
+    let content_block_type = content_block.get("type").unwrap().as_str().unwrap();
+    assert_eq!(content_block_type, "tool_call");
+    assert_eq!(content_block.get("id").unwrap().as_str().unwrap(), tool_id);
+    assert_eq!(
+        content_block.get("name").unwrap().as_str().unwrap(),
+        "get_weather"
+    );
+    assert_eq!(
+        content_block.get("arguments").unwrap().as_str().unwrap(),
+        arguments
+    );
+
+    let tool_params: Value =
+        serde_json::from_str(result.get("tool_params").unwrap().as_str().unwrap()).unwrap();
+    assert_eq!(tool_params["tool_choice"], "required");
+    assert_eq!(tool_params["parallel_tool_calls"], false);
+
+    let tools_available = tool_params["tools_available"].as_array().unwrap();
+    assert_eq!(tools_available.len(), 1);
+    let tool = tools_available.first().unwrap();
+    assert_eq!(tool["name"], "get_weather");
+    assert_eq!(
+        tool["description"],
+        "Get the current weather in a given location"
+    );
+    assert_eq!(tool["strict"], false);
+
+    let tool_parameters = tool["parameters"].as_object().unwrap();
+    assert_eq!(tool_parameters["type"], "object");
+    assert!(tool_parameters.get("properties").is_some());
+    assert!(tool_parameters.get("required").is_some());
+    assert_eq!(tool_parameters["additionalProperties"], false);
+
+    let properties = tool_parameters["properties"].as_object().unwrap();
+    assert!(properties.contains_key("location"));
+    assert!(properties.contains_key("units"));
+
+    let location = properties["location"].as_object().unwrap();
+    assert_eq!(location["type"], "string");
+
+    let units = properties["units"].as_object().unwrap();
+    assert_eq!(units["type"], "string");
+    let units_enum = units["enum"].as_array().unwrap();
+    assert_eq!(units_enum.len(), 2);
+    assert!(units_enum.contains(&json!("fahrenheit")));
+    assert!(units_enum.contains(&json!("celsius")));
+
+    let required = tool_parameters["required"].as_array().unwrap();
+    assert!(required.contains(&json!("location")));
+
+    // Check if ClickHouse is correct - ModelInference Table
+    let result = select_model_inferences_clickhouse(&clickhouse, inference_id)
+        .await
+        .unwrap();
+
+    let id = result.get("id").unwrap().as_str().unwrap();
+    let _ = Uuid::parse_str(id).unwrap();
+
+    let inference_id_result = result.get("inference_id").unwrap().as_str().unwrap();
+    let inference_id_result = Uuid::parse_str(inference_id_result).unwrap();
+    assert_eq!(inference_id_result, inference_id);
+
+    let input: Value =
+        serde_json::from_str(result.get("input").unwrap().as_str().unwrap()).unwrap();
+    assert_eq!(input, correct_input);
+
+    let output_clickhouse_model = result.get("output").unwrap().as_str().unwrap();
+    let output_clickhouse_model: Vec<Value> =
+        serde_json::from_str(output_clickhouse_model).unwrap();
+    assert_eq!(output_clickhouse_model.len(), 1);
+    let content_block = output_clickhouse_model.first().unwrap();
+    let content_block_type = content_block.get("type").unwrap().as_str().unwrap();
+    assert_eq!(content_block_type, "tool_call");
+
+    let _ = content_block.get("id").unwrap().as_str().unwrap();
+
+    let name = content_block.get("name").unwrap().as_str().unwrap();
+    assert_eq!(name, "get_weather");
+
+    let arguments = content_block.get("arguments").unwrap().as_str().unwrap();
+    let arguments: Value = serde_json::from_str(arguments).unwrap();
+    let arguments = arguments.as_object().unwrap();
+    assert!(arguments.len() == 1 || arguments.len() == 2);
+    let location = arguments.get("location").unwrap().as_str().unwrap();
+    assert_eq!(location, "Tokyo");
+    if arguments.len() == 2 {
+        let units = arguments.get("units").unwrap().as_str().unwrap();
+        assert!(units == "fahrenheit" || units == "celsius");
+    }
+
+    let raw_response = result.get("raw_response").unwrap().as_str().unwrap();
+    assert!(raw_response.contains("get_weather"));
+    // Check if raw_response is valid JSONL
+    for line in raw_response.lines() {
+        assert!(serde_json::from_str::<Value>(line).is_ok());
+    }
+
+    let input_tokens = result.get("input_tokens").unwrap().as_u64().unwrap();
+    let output_tokens = result.get("output_tokens").unwrap().as_u64().unwrap();
+
+    // NB: Azure doesn't support input/output tokens during streaming
+    if provider.variant_name != "azure" {
+        assert!(input_tokens > 0);
+        assert!(output_tokens > 0);
+    } else {
+        assert_eq!(input_tokens, 0);
+        assert_eq!(output_tokens, 0);
+    }
+
+    let response_time_ms = result.get("response_time_ms").unwrap().as_u64().unwrap();
+    assert!(response_time_ms > 0);
+
+    let ttft_ms = result.get("ttft_ms").unwrap().as_u64().unwrap();
+    assert!(ttft_ms > 100);
+    assert!(ttft_ms <= response_time_ms);
 }
