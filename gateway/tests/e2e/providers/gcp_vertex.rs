b83d41af8419f312e875fa570c0c8883206e6af8
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2024-08-21 13:56:17 -0400

    Update

--- gateway/tests/e2e/providers/gcp_vertex.rs
+++ gateway/tests/e2e/providers/gcp_vertex.rs
@@ -13,12 +13,15 @@ use crate::providers::common::{E2ETestProvider, E2ETestProviders};
 crate::generate_provider_tests!(get_providers);
 
 async fn get_providers() -> E2ETestProviders {
-    // TODO: flash + pro
-    let provider = E2ETestProvider {
-        variant_name: "gcp-vertex-gemini".to_string(),
+    let provider_flash = E2ETestProvider {
+        variant_name: "gcp-vertex-gemini-flash".to_string(),
     };
 
-    E2ETestProviders::with_provider(provider)
+    let provider_pro = E2ETestProvider {
+        variant_name: "gcp-vertex-gemini-pro".to_string(),
+    };
+
+    E2ETestProviders::with_providers(vec![provider_flash, provider_pro])
 }
 /// GCP Vertex E2E tests
 ///
@@ -31,291 +34,6 @@ async fn get_providers() -> E2ETestProviders {
 /// TODOs (#81):
 /// - tool calling
 /// - other API parameters (temp, max_tokens, etc.)
-
-/// This test checks that streaming inference works as expected.
-#[tokio::test]
-async fn test_streaming() {
-    let client = Client::new();
-    let episode_id = Uuid::now_v7();
-
-    let payload = json!({
-        "function_name": "basic_test",
-        "variant_name": "gcp-vertex-gemini",
-        "episode_id": episode_id,
-        "input":
-        {
-           "system": {"assistant_name": "AskJeeves"},
-           "messages": [
-            {
-                "role": "user",
-                "content": "Hello, world!"
-            }
-        ]},
-        "stream": true,
-    });
-
-    let mut event_source = client
-        .post(get_gateway_endpoint("/inference"))
-        .json(&payload)
-        .eventsource()
-        .unwrap();
-    let mut chunks = vec![];
-    while let Some(event) = event_source.next().await {
-        let event = event.unwrap();
-        match event {
-            Event::Open => continue,
-            Event::Message(message) => {
-                if message.data == "[DONE]" {
-                    break;
-                }
-                chunks.push(message.data);
-            }
-        }
-    }
-    let mut inference_id = None;
-    let mut full_content = String::new();
-    for chunk in chunks {
-        let chunk_json: Value = serde_json::from_str(&chunk).unwrap();
-        let content_blocks = chunk_json.get("content").unwrap().as_array().unwrap();
-        if content_blocks.is_empty() {
-            continue;
-        }
-        let content_block = content_blocks.first().unwrap();
-        let content = content_block.get("text").unwrap().as_str().unwrap();
-        full_content.push_str(content);
-        inference_id = Some(
-            Uuid::parse_str(chunk_json.get("inference_id").unwrap().as_str().unwrap()).unwrap(),
-        );
-    }
-    let inference_id = inference_id.unwrap();
-    // Sleep for 1 second to allow time for data to be inserted into ClickHouse (trailing writes from API)
-    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-
-    // Check ClickHouse
-    let clickhouse = get_clickhouse().await;
-
-    // First, check Inference table
-    let result = select_inference_clickhouse(&clickhouse, inference_id)
-        .await
-        .unwrap();
-    let id = result.get("id").unwrap().as_str().unwrap();
-    let id_uuid = Uuid::parse_str(id).unwrap();
-    assert_eq!(id_uuid, inference_id);
-    let input: Value =
-        serde_json::from_str(result.get("input").unwrap().as_str().unwrap()).unwrap();
-    let correct_input = json!({
-        "system": {"assistant_name": "AskJeeves"},
-        "messages": [
-            {
-                "role": "user",
-                "content": [{
-                    "type": "text",
-                    "value": "Hello, world!"
-                }]
-            }
-        ]
-    });
-    assert_eq!(input, correct_input);
-    let content_blocks = result.get("output").unwrap().as_str().unwrap();
-    // Check that content_blocks is a list of blocks length 1
-    let content_blocks: Vec<Value> = serde_json::from_str(content_blocks).unwrap();
-    assert_eq!(content_blocks.len(), 1);
-    let content_block = content_blocks.first().unwrap();
-    // Check the type and content in the block
-    let content_block_type = content_block.get("type").unwrap().as_str().unwrap();
-    assert_eq!(content_block_type, "text");
-    let clickhouse_content = content_block.get("text").unwrap().as_str().unwrap();
-    assert_eq!(clickhouse_content, full_content);
-    // Check that episode_id is here and correct
-    let retrieved_episode_id = result.get("episode_id").unwrap().as_str().unwrap();
-    let retrieved_episode_id = Uuid::parse_str(retrieved_episode_id).unwrap();
-    assert_eq!(retrieved_episode_id, episode_id);
-    // Check the variant name
-    let variant_name = result.get("variant_name").unwrap().as_str().unwrap();
-    assert_eq!(variant_name, "gcp-vertex-gemini");
-    // Check the processing time
-    let processing_time_ms = result.get("processing_time_ms").unwrap().as_u64().unwrap();
-    assert!(processing_time_ms > 0);
-
-    // Check the ModelInference Table
-    let result = select_model_inferences_clickhouse(&clickhouse, inference_id)
-        .await
-        .unwrap();
-    let inference_id_result = result.get("inference_id").unwrap().as_str().unwrap();
-    let inference_id_result = Uuid::parse_str(inference_id_result).unwrap();
-    assert_eq!(inference_id_result, inference_id);
-    let input: Value =
-        serde_json::from_str(result.get("input").unwrap().as_str().unwrap()).unwrap();
-    assert_eq!(input, correct_input);
-    let output = result.get("output").unwrap().as_str().unwrap();
-    let content_blocks: Vec<Value> = serde_json::from_str(output).unwrap();
-    assert_eq!(content_blocks.len(), 1);
-    let content_block = content_blocks.first().unwrap();
-    let content_block_type = content_block.get("type").unwrap().as_str().unwrap();
-    assert_eq!(content_block_type, "text");
-    let clickhouse_content = content_block.get("text").unwrap().as_str().unwrap();
-    assert_eq!(clickhouse_content, full_content);
-
-    let input_tokens = result.get("input_tokens").unwrap().as_u64().unwrap();
-    assert!(input_tokens > 5);
-    let output_tokens = result.get("output_tokens").unwrap().as_u64().unwrap();
-    assert!(output_tokens > 5);
-    let response_time_ms = result.get("response_time_ms").unwrap().as_u64().unwrap();
-    assert!(response_time_ms > 0);
-    let raw_response = result.get("raw_response").unwrap().as_str().unwrap();
-    // Check if raw_response is valid JSONL
-    for line in raw_response.lines() {
-        let _: Value = serde_json::from_str(line).expect("Each line should be valid JSON");
-    }
-    let response_time_ms = result.get("response_time_ms").unwrap().as_u64().unwrap();
-    assert!(response_time_ms > 100);
-    let ttft_ms = result.get("ttft_ms").unwrap().as_u64().unwrap();
-    assert!(ttft_ms > 100);
-    assert!(ttft_ms <= response_time_ms);
-}
-
-#[tokio::test]
-async fn test_tool_call() {
-    let episode_id = Uuid::now_v7();
-
-    let payload = json!({
-        "function_name": "weather_helper",
-        "episode_id": episode_id,
-        "input":{
-            "system": {"assistant_name": "AskJeeves"},
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "Hi I'm visiting Brooklyn from Brazil. What's the weather?"
-                }
-            ]},
-        "stream": false,
-        "variant_name": "gcp-vertex",
-    });
-    let response = Client::new()
-        .post(get_gateway_endpoint("/inference"))
-        .json(&payload)
-        .send()
-        .await
-        .unwrap();
-    // Check Response is OK, then fields in order
-    assert_eq!(response.status(), StatusCode::OK);
-    let response_json = response.json::<Value>().await.unwrap();
-    // No output schema so parsed content should not be in response
-    assert!(response_json.get("parsed_content").is_none());
-    // Check that inference_id is here
-    let inference_id = response_json.get("inference_id").unwrap().as_str().unwrap();
-    let inference_id = Uuid::parse_str(inference_id).unwrap();
-    // Check that raw_content is same as content
-    let content_blocks: &Vec<Value> = response_json.get("output").unwrap().as_array().unwrap();
-    assert_eq!(content_blocks.len(), 1);
-    let content_block = content_blocks.first().unwrap();
-    let content_block_type = content_block.get("type").unwrap().as_str().unwrap();
-    assert_eq!(content_block_type, "tool_call");
-    let arguments = content_block.get("arguments").unwrap().as_str().unwrap();
-    let _: Value = serde_json::from_str(arguments).unwrap();
-    content_block.get("id").unwrap().as_str().unwrap();
-    let name = content_block.get("name").unwrap().as_str().unwrap();
-    assert_eq!(name, "get_weather");
-    let parsed_name = content_block.get("parsed_name").unwrap().as_str().unwrap();
-    assert_eq!(parsed_name, "get_weather");
-    // This could fail if the LLM fails to return correct arguments (similarly in the Inference table)
-    content_block.get("parsed_arguments").unwrap();
-
-    // Check that type is "chat"
-    // Check that usage is correct
-    let usage = response_json.get("usage").unwrap();
-    let usage = usage.as_object().unwrap();
-    let prompt_tokens = usage.get("prompt_tokens").unwrap().as_u64().unwrap();
-    let completion_tokens = usage.get("completion_tokens").unwrap().as_u64().unwrap();
-    assert!(prompt_tokens > 10);
-    assert!(completion_tokens > 0);
-    // Sleep for 1 second to allow time for data to be inserted into ClickHouse (trailing writes from API)
-    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-
-    // Check ClickHouse
-    let clickhouse = get_clickhouse().await;
-    let result = select_inference_clickhouse(&clickhouse, inference_id)
-        .await
-        .unwrap();
-    let id = result.get("id").unwrap().as_str().unwrap();
-    let id_uuid = Uuid::parse_str(id).unwrap();
-    assert_eq!(id_uuid, inference_id);
-    let input: Value =
-        serde_json::from_str(result.get("input").unwrap().as_str().unwrap()).unwrap();
-    let correct_input: Value = json!(
-        {
-            "system": {
-                "assistant_name": "AskJeeves"
-            },
-            "messages": [
-                {
-                    "role": "user",
-                    "content": [{"type": "text", "value": "Hi I'm visiting Brooklyn from Brazil. What's the weather?"}]
-                }
-            ]
-        }
-    );
-    assert_eq!(input, correct_input);
-    // Check that content blocks are correct
-    let content_blocks = result.get("output").unwrap().as_str().unwrap();
-    let content_blocks: Vec<Value> = serde_json::from_str(content_blocks).unwrap();
-    assert_eq!(content_blocks.len(), 1);
-    let content_block = content_blocks.first().unwrap();
-    let content_block_type = content_block.get("type").unwrap().as_str().unwrap();
-    assert_eq!(content_block_type, "tool_call");
-    // Check that the tool call is correctly stored
-    let arguments = content_block.get("arguments").unwrap().as_str().unwrap();
-    let _: Value = serde_json::from_str(arguments).unwrap();
-    content_block.get("id").unwrap().as_str().unwrap();
-    let name = content_block.get("name").unwrap().as_str().unwrap();
-    assert_eq!(name, "get_weather");
-    let parsed_name = content_block.get("parsed_name").unwrap().as_str().unwrap();
-    assert_eq!(parsed_name, "get_weather");
-    content_block.get("parsed_arguments").unwrap();
-    // Check that episode_id is here and correct
-    let retrieved_episode_id = result.get("episode_id").unwrap().as_str().unwrap();
-    let retrieved_episode_id = Uuid::parse_str(retrieved_episode_id).unwrap();
-    assert_eq!(retrieved_episode_id, episode_id);
-    // Check the variant name
-    let variant_name = result.get("variant_name").unwrap().as_str().unwrap();
-    assert_eq!(variant_name, "gcp-vertex");
-
-    // Check the ModelInference Table
-    let result = select_model_inferences_clickhouse(&clickhouse, inference_id)
-        .await
-        .unwrap();
-    let id = result.get("id").unwrap().as_str().unwrap();
-    let _ = Uuid::parse_str(id).unwrap();
-    let inference_id_result = result.get("inference_id").unwrap().as_str().unwrap();
-    let inference_id_result = Uuid::parse_str(inference_id_result).unwrap();
-    assert_eq!(inference_id_result, inference_id);
-
-    let input_result = result.get("input").unwrap().as_str().unwrap();
-    let input_result: Value = serde_json::from_str(input_result).unwrap();
-    assert_eq!(input_result, correct_input);
-    let output = result.get("output").unwrap().as_str().unwrap();
-    let content_blocks: Vec<Value> = serde_json::from_str(output).unwrap();
-    assert_eq!(content_blocks.len(), 1);
-    let content_block = content_blocks.first().unwrap();
-    let content_block_type = content_block.get("type").unwrap().as_str().unwrap();
-    assert_eq!(content_block_type, "tool_call");
-    // Check that the tool call is correctly stored (no parsing here)
-    let arguments = content_block.get("arguments").unwrap().as_str().unwrap();
-    let _: Value = serde_json::from_str(arguments).unwrap();
-    content_block.get("id").unwrap().as_str().unwrap();
-    let name = content_block.get("name").unwrap().as_str().unwrap();
-    assert_eq!(name, "get_weather");
-
-    let input_tokens = result.get("input_tokens").unwrap().as_u64().unwrap();
-    assert!(input_tokens > 10);
-    let output_tokens = result.get("output_tokens").unwrap().as_u64().unwrap();
-    assert!(output_tokens > 0);
-    let response_time_ms = result.get("response_time_ms").unwrap().as_u64().unwrap();
-    assert!(response_time_ms > 0);
-    assert!(result.get("ttft_ms").unwrap().is_null());
-    result.get("raw_response").unwrap().as_str().unwrap();
-}
 
 #[tokio::test]
 async fn test_json_request_pro() {
@@ -701,7 +419,7 @@ async fn test_tool_call_streaming() {
                 }
             ]},
         "stream": true,
-        "variant_name": "gcp-vertex",
+        "variant_name": "gcp-vertex-gemini-pro",
     });
     let mut event_source = Client::new()
         .post(get_gateway_endpoint("/inference"))
@@ -821,7 +539,7 @@ async fn test_tool_call_streaming() {
     assert_eq!(retrieved_episode_id, episode_id);
     // Check the variant name
     let variant_name = result.get("variant_name").unwrap().as_str().unwrap();
-    assert_eq!(variant_name, "gcp-vertex");
+    assert_eq!(variant_name, "gcp-vertex-gemini-pro");
     // Check the tool_params
     let tool_params = result.get("tool_params").unwrap().as_str().unwrap();
     let tool_params: Value = serde_json::from_str(tool_params).unwrap();
