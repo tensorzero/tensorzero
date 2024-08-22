ca23d1e3f5ad6964035dfb5d2187d93332875821
Viraj Mehta <virajmehta@users.noreply.github.com>
2024-08-22 13:23:34 -0400

    Implements vLLM inference provider (#170)
    * start of vLLM implementation
    
    * wired the vllm provider to the inference code
    
    * added initial vllm integration test
    
    * vllm works except integration tests
    
    * removed tools from vLLM integration
    
    * vllm e2e tests pass
    
    * cleaned up vllm tests
    
    * made only openai changes that were needed
    
    * cleaned up
    
    * remove id changes
    
    * fixed bug in variant preparation and GCP vertex, changed model to phi
    
    * fixed merge queue ci to have info needed for vLLM
    
    * updated toml file and ci to point to RunPod
    
    * Update tests
    
    * Update tests
    
    * changed API base in CI to a secret
    
    * Update tests
    
    ---------
    
    Co-authored-by: Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>

--- gateway/src/inference/providers/openai.rs
+++ gateway/src/inference/providers/openai.rs
@@ -465,8 +465,8 @@ impl<'a> From<&'a ToolChoice> for OpenAIToolChoice<'a> {
 }
 
 #[derive(Debug, Serialize)]
-struct StreamOptions {
-    include_usage: bool,
+pub(super) struct StreamOptions {
+    pub(super) include_usage: bool,
 }
 
 /// This struct defines the supported parameters for the OpenAI API
