525700a0604e16fcb1792779bb6363642dd85420
Viraj Mehta <viraj@tensorzero.com>
2024-08-23 15:52:57 -0400

    fixed mistral tests

--- gateway/tests/e2e/providers/mistral.rs
+++ gateway/tests/e2e/providers/mistral.rs
@@ -3,7 +3,19 @@ use crate::providers::common::{E2ETestProvider, E2ETestProviders};
 crate::generate_provider_tests!(get_providers);
 
 async fn get_providers() -> E2ETestProviders {
-    E2ETestProviders::with_provider(E2ETestProvider {
+    let providers = vec![E2ETestProvider {
         variant_name: "mistral".to_string(),
-    })
+    }];
+    E2ETestProviders {
+        simple_inference: providers.clone(),
+        streaming_inference: providers.clone(),
+        tool_use_inference: providers.clone(),
+        tool_use_streaming_inference: providers.clone(),
+        tool_multi_turn_inference: providers.clone(),
+        tool_multi_turn_streaming_inference: providers.clone(),
+        dynamic_tool_use_inference: providers.clone(),
+        dynamic_tool_use_streaming_inference: providers.clone(),
+        json_mode_inference: providers.clone(),
+        json_mode_streaming_inference: providers.clone(),
+    }
 }
