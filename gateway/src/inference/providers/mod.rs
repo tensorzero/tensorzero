3ce2cd0d2bc7b7dac423966b3f1434c2c7bb4354
Viraj Mehta <viraj@tensorzero.com>
2024-09-11 21:13:43 -0400

    added what seems to be a compliant implementation of GCP vertex anthropic, no integration tests yet

--- gateway/src/inference/providers/mod.rs
+++ gateway/src/inference/providers/mod.rs
@@ -6,6 +6,7 @@ pub mod common;
 #[cfg(any(test, feature = "e2e_tests"))]
 pub mod dummy;
 pub mod fireworks;
+pub mod gcp_vertex_anthropic;
 pub mod gcp_vertex_gemini;
 pub mod mistral;
 pub mod openai;
