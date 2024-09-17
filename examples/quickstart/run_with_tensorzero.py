3efa800f1e53f757f1434ed9ba9ec7d2f8d2cd28
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2024-09-17 18:13:28 -0400

    Update

--- examples/quickstart/run_with_tensorzero.py
+++ examples/quickstart/run_with_tensorzero.py
@@ -6,26 +6,39 @@ from tensorzero import AsyncTensorZeroGateway
 async def run_with_tensorzero(topic):
     async with AsyncTensorZeroGateway("http://localhost:3000") as client:
         # Run the inference API call...
-        inference_result = await client.inference(
+        result = await client.inference(
             function_name="generate_haiku",
             input={
                 "messages": [
-                    {"role": "user", "content": {"topic": topic}},
+                    {
+                        "role": "user",
+                        "content": "Write a haiku about artificial intelligence.",
+                    },
                 ],
             },
         )
 
-        print(inference_result)
-
-        # ... and associate feedback to that inference using its ID
-        feedback_result = await client.feedback(
-            metric_name="thumbs_up",
-            inference_id=inference_result.inference_id,
-            value=True,  # 👍
-        )
-
-        print(feedback_result)
+        print(result)
 
 
 if __name__ == "__main__":
     asyncio.run(run_with_tensorzero("artificial intelligence"))
+
+
+# from tensorzero import TensorZeroGateway
+
+
+# result = TensorZeroGateway("http://localhost:3000").inference(
+#     # Since our function has a single variant, the gateway will automatically select it
+#     function_name="generate_haiku",
+#     input={
+#         "messages": [
+#             {
+#                 "role": "user",
+#                 "content": "Write a haiku about artificial intelligence.",
+#             }
+#         ]
+#     },
+# )
+
+# print(result)
