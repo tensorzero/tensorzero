fd5cd0973fab24e9ecb1850d2e877e05d2430f81
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2024-09-18 16:49:15 -0400

    Update

--- examples/quickstart/run_with_tensorzero.py
+++ examples/quickstart/run_with_tensorzero.py
@@ -1,44 +1,15 @@
-import asyncio
+from tensorzero import TensorZeroGateway
 
-from tensorzero import AsyncTensorZeroGateway
+result = TensorZeroGateway("http://localhost:3000").inference(
+    function_name="generate_haiku",
+    input={
+        "messages": [
+            {
+                "role": "user",
+                "content": "Write a haiku about artificial intelligence.",
+            }
+        ]
+    },
+)
 
-
-async def run_with_tensorzero(topic):
-    async with AsyncTensorZeroGateway("http://localhost:3000") as client:
-        # Run the inference API call...
-        result = await client.inference(
-            function_name="generate_haiku",
-            input={
-                "messages": [
-                    {
-                        "role": "user",
-                        "content": "Write a haiku about artificial intelligence.",
-                    },
-                ],
-            },
-        )
-
-        print(result)
-
-
-if __name__ == "__main__":
-    asyncio.run(run_with_tensorzero("artificial intelligence"))
-
-
-# from tensorzero import TensorZeroGateway
-
-
-# result = TensorZeroGateway("http://localhost:3000").inference(
-#     # Since our function has a single variant, the gateway will automatically select it
-#     function_name="generate_haiku",
-#     input={
-#         "messages": [
-#             {
-#                 "role": "user",
-#                 "content": "Write a haiku about artificial intelligence.",
-#             }
-#         ]
-#     },
-# )
-
-# print(result)
+print(result)
