4ca6b349352b3d457ed96ef85d601d32598f115d
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2024-09-24 15:52:35 -0400

    Update examples for Tutorial V2 (#331)
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update

--- examples/quickstart/run_with_openai.py
+++ examples/quickstart/run_with_openai.py
@@ -1,21 +1,13 @@
 from openai import OpenAI
 
+response = OpenAI().chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {
+            "role": "user",
+            "content": "Write a haiku about artificial intelligence.",
+        }
+    ],
+)
 
-def run_with_openai(topic):
-    client = OpenAI()
-
-    result = client.chat.completions.create(
-        model="gpt-4o-mini-2024-07-18",
-        messages=[
-            {
-                "role": "user",
-                "content": f"Write a haiku about '{topic}'. Don't write anything else.",
-            }
-        ],
-    )
-
-    print(result)
-
-
-if __name__ == "__main__":
-    run_with_openai("artificial intelligence")
+print(response)
--- examples/quickstart/run_with_tensorzero.py
+++ examples/quickstart/run_with_tensorzero.py
@@ -1,31 +1,15 @@
-import asyncio
+from tensorzero import TensorZeroGateway
 
-from tensorzero import AsyncTensorZeroGateway
+response = TensorZeroGateway("http://localhost:3000").inference(
+    function_name="generate_haiku",
+    input={
+        "messages": [
+            {
+                "role": "user",
+                "content": "Write a haiku about artificial intelligence.",
+            }
+        ]
+    },
+)
 
-
-async def run_with_tensorzero(topic):
-    async with AsyncTensorZeroGateway("http://localhost:3000") as client:
-        # Run the inference API call...
-        inference_result = await client.inference(
-            function_name="generate_haiku",
-            input={
-                "messages": [
-                    {"role": "user", "content": {"topic": topic}},
-                ],
-            },
-        )
-
-        print(inference_result)
-
-        # ... and associate feedback to that inference using its ID
-        feedback_result = await client.feedback(
-            metric_name="thumbs_up",
-            inference_id=inference_result.inference_id,
-            value=True,  # 👍
-        )
-
-        print(feedback_result)
-
-
-if __name__ == "__main__":
-    asyncio.run(run_with_tensorzero("artificial intelligence"))
+print(response)
