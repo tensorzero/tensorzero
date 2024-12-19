9e649605967df8ffc3fde40dc42b0aa193aeb22f
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2024-12-19 11:09:50 -0500

    Fix favicon

--- dashboard/app/root.tsx
+++ dashboard/app/root.tsx
@@ -22,6 +22,11 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "icon",
+    type: "image/svg+xml",
+    href: "/favicon.svg",
+  },
 ];
 
 export async function loader() {
