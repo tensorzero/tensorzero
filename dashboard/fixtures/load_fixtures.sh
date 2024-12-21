b1822a50348e0e0d003ccedf3735bb8a1f17cf0b
Viraj Mehta <viraj@tensorzero.com>
2024-12-20 20:06:46 -0500

    removed old dashboard

--- dashboard/fixtures/load_fixtures.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-clickhouse-client --host $CLICKHOUSE_HOST --database tensorzero --query "INSERT INTO JsonInference FORMAT CSV" < json_inference_examples.csv
-clickhouse-client --host $CLICKHOUSE_HOST --database tensorzero --query "INSERT INTO BooleanMetricFeedback FORMAT CSV" < boolean_metric_feedback_examples.csv
--- dashboard/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import wasm from "vite-plugin-wasm";
-
-export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths(), wasm()],
-});
