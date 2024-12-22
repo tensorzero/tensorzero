25beaa3f17dc28606fd1dd5cd3e9c93ba829a257
Viraj Mehta <viraj@tensorzero.com>
2024-12-21 20:03:35 -0500

    added set -e

--- dashboard/fixtures/load_fixtures.sh
+++ dashboard/fixtures/load_fixtures.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+set -e
 clickhouse-client --host $CLICKHOUSE_HOST --database tensorzero --query "INSERT INTO JsonInference FORMAT CSV" < json_inference_examples.csv
 clickhouse-client --host $CLICKHOUSE_HOST --database tensorzero --query "INSERT INTO BooleanMetricFeedback FORMAT CSV" < boolean_metric_feedback_examples.csv
+echo "Fixtures loaded; this container will now exit with status 0"
