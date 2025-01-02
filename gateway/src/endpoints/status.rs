8cab4d6ebd0e60a55f30889c732d7b15756c109b
Viraj Mehta <viraj@tensorzero.com>
2025-01-02 18:45:46 -0300

    bumped version to 2025.01.0, jklol

--- gateway/src/endpoints/status.rs
+++ gateway/src/endpoints/status.rs
@@ -5,7 +5,7 @@ use axum::http::StatusCode;
 use axum::response::Json;
 use serde_json::{json, Value};
 
-const TENSORZERO_VERSION: &str = "2025.1.0";
+const TENSORZERO_VERSION: &str = "2025.01.0";
 
 /// A handler for a simple liveness check
 #[debug_handler]
