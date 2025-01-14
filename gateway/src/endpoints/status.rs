e871189d191fe9b9379d240356dbaa2e83b6d321
Viraj Mehta <virajmehta@users.noreply.github.com>
2025-01-14 17:38:09 -0500

    bumped version to 2025.01.2 (#762)

--- gateway/src/endpoints/status.rs
+++ gateway/src/endpoints/status.rs
@@ -5,7 +5,7 @@ use axum::http::StatusCode;
 use axum::response::Json;
 use serde_json::{json, Value};
 
-const TENSORZERO_VERSION: &str = "2025.01.1";
+const TENSORZERO_VERSION: &str = "2025.01.2";
 
 /// A handler for a simple liveness check
 #[debug_handler]
