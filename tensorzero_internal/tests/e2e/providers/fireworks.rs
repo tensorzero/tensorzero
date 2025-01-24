d934756532ac3f3f16478ee0863adbcd96d79377
Alberto Merino <amerinor01@gmail.com>
2025-01-24 17:58:13 +0100

    WIP: change to Llama model

--- tensorzero_internal/tests/e2e/providers/fireworks.rs
+++ tensorzero_internal/tests/e2e/providers/fireworks.rs
@@ -15,31 +15,30 @@ async fn get_providers() -> E2ETestProviders {
 
     let providers = vec![E2ETestProvider {
         variant_name: "fireworks".to_string(),
-        model_name: "llama3.1-8b-instruct-fireworks".into(),
+        model_name: "llama3.1-70b-instruct-fireworks".into(),
         model_provider_name: "fireworks".into(),
         credentials: HashMap::new(),
     }];
 
     let inference_params_providers = vec![E2ETestProvider {
         variant_name: "fireworks-dynamic".to_string(),
-        model_name: "llama3.1-8b-instruct-fireworks-dynamic".into(),
+        model_name: "llama3.1-70b-instruct-fireworks-dynamic".into(),
         model_provider_name: "fireworks".into(),
         credentials,
     }];
 
-    // NOTE: FireFunction might not be available serverlessly anymore so we have temporarily disabled it
-    // let tool_providers = vec![E2ETestProvider {
-    //     variant_name: "fireworks-firefunction".to_string(),
-    //     model_name: "firefunction-v2".into(),
-    //     model_provider_name: "fireworks".into(),
-    //     credentials: HashMap::new(),
-    // }];
-    let tool_providers = vec![];
+    let tool_providers = vec![E2ETestProvider {
+        variant_name: "fireworks".to_string(),
+        model_name: "llama3.1-70b-instruct-fireworks".into(),
+        model_provider_name: "fireworks".into(),
+        credentials: HashMap::new(),
+    }];
+    //let tool_providers = vec![];
 
     let json_providers = vec![
         E2ETestProvider {
             variant_name: "fireworks".to_string(),
-            model_name: "llama3.1-8b-instruct-fireworks".into(),
+            model_name: "llama3.1-70b-instruct-fireworks".into(),
             model_provider_name: "fireworks".into(),
             credentials: HashMap::new(),
         },
@@ -54,7 +53,7 @@ async fn get_providers() -> E2ETestProviders {
     #[cfg(feature = "e2e_tests")]
     let shorthand_providers = vec![E2ETestProvider {
         variant_name: "fireworks-shorthand".to_string(),
-        model_name: "fireworks::accounts/fireworks/models/llama-v3p1-8b-instruct".into(),
+        model_name: "fireworks::accounts/fireworks/models/llama-v3p3-70b-instruct".into(),
         model_provider_name: "fireworks".into(),
         credentials: HashMap::new(),
     }];
