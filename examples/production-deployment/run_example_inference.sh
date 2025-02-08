8eb33e8c42eb0ef8065189cdb92c6cf5d58b0709
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2025-02-08 18:39:55 -0500

    Make production tests actually fail

--- examples/production-deployment/run_example_inference.sh
+++ examples/production-deployment/run_example_inference.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 curl -X POST http://localhost:3000/inference \
+  --fail-with-body \
   -H "Content-Type: application/json" \
   -d '{
     "function_name": "simple_llm_call",
@@ -13,12 +14,13 @@ curl -X POST http://localhost:3000/inference \
         }
       ]
     }
-  }'
+  }' || exit 1
 
 curl -X POST http://localhost:3000/feedback \
+  --fail-with-body \
   -H "Content-Type: application/json" \
   -d '{
     "metric_name": "task_success",
     "inference_id": "00000000-0000-0000-0000-000000000000",
     "value": true
-  }'
+  }' || exit 1
