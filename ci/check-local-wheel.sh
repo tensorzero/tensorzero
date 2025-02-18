d21c520dd931d3300b366bbb2d7739bd3d2af207
Aaron Hill <aaron@tensorzero.com>
2025-02-18 15:21:50 -0500

    Use 'uv' instead of sourcing venv

--- ci/check-local-wheel.sh
+++ ci/check-local-wheel.sh
@@ -2,6 +2,5 @@
 set -euxo pipefail
 
 uv venv .test-venv
-source .test-venv/bin/activate
 uv pip install $1
-python -c 'import tensorzero; print(tensorzero.tensorzero.__file__)'
+uv run python -c 'import tensorzero; print(tensorzero.tensorzero.__file__)'
