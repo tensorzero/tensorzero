3f3ea71d7782b9b6de1a7f90dc0dcdfe2f7f0950
Viraj Mehta <viraj@tensorzero.com>
2025-03-07 11:45:39 -0500

    added handling for missing functions in config;

--- ui/app/components/function/FunctionInfo.tsx
+++ ui/app/components/function/FunctionInfo.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router";
 import { Code } from "~/components/ui/code";
+import { useConfig } from "~/context/config";
+import { AlertDialog } from "~/components/ui/AlertDialog";
 
 type FunctionInfoProps = {
   functionName: string;
@@ -10,12 +12,22 @@ export function FunctionInfo({
   functionName,
   functionType,
 }: FunctionInfoProps) {
+  const config = useConfig();
+  const functionConfig = config.functions[functionName];
+
   return (
     <>
       <dd>
-        <Link to={`/observability/functions/${functionName}`}>
-          <Code>{functionName}</Code>
-        </Link>
+        {functionConfig ? (
+          <Link to={`/observability/functions/${functionName}`}>
+            <Code>{functionName}</Code>
+          </Link>
+        ) : (
+          <AlertDialog
+            message="This function is not present in your configuration file"
+            trigger={<Code>{functionName}</Code>}
+          />
+        )}
       </dd>
       <Code>{functionType}</Code>
     </>
