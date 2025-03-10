34a9179a5033df72f9d4ea110c2058fdc3ebea27
Viraj Mehta <viraj@tensorzero.com>
2025-03-10 12:04:05 -0400

    added tests for internal parameter

--- tensorzero-internal/src/endpoints/mod.rs
+++ tensorzero-internal/src/endpoints/mod.rs
@@ -10,7 +10,10 @@ pub mod inference;
 pub mod openai_compatible;
 pub mod status;
 
-fn validate_tags(tags: &HashMap<String, String>) -> Result<(), Error> {
+fn validate_tags(tags: &HashMap<String, String>, internal: bool) -> Result<(), Error> {
+    if internal {
+        return Ok(());
+    }
     for tag_name in tags.keys() {
         if tag_name.starts_with("tensorzero::") {
             return Err(Error::new(ErrorDetails::InvalidRequest {
@@ -28,8 +31,10 @@ mod tests {
     #[test]
     fn test_validate_tags() {
         let mut tags = HashMap::new();
-        assert!(validate_tags(&tags).is_ok());
+        assert!(validate_tags(&tags, false).is_ok());
         tags.insert("tensorzero::test".to_string(), "test".to_string());
-        assert!(validate_tags(&tags).is_err());
+        assert!(validate_tags(&tags, false).is_err());
+        // once we're in internal mode, we can have tags that start with "tensorzero::"
+        assert!(validate_tags(&tags, true).is_ok());
     }
 }
