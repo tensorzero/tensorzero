c91d502002fde002019c64b4796340b7d67d1361
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2025-03-09 20:50:32 -0400

    Update

--- clients/openai-node/eslint.config.js
+++ clients/openai-node/eslint.config.js
@@ -3,10 +3,7 @@ import tseslint from "typescript-eslint";
 
 export default [
   {
-    ignores: [
-      "**/node_modules/**",
-      "**/dist/**",
-    ],
+    ignores: ["**/node_modules/**", "**/dist/**"],
   },
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
