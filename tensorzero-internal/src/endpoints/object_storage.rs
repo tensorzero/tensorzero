69799884b26fb4c019f7ba4aba44b186bace01e8
Aaron Hill <aaron@tensorzero.com>
2025-03-12 10:48:13 -0400

    Address review comments

--- tensorzero-internal/src/endpoints/object_storage.rs
+++ tensorzero-internal/src/endpoints/object_storage.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use axum::{
     extract::{Query, State},
     Json,
@@ -22,8 +24,18 @@ pub struct PathParams {
     storage_path: String,
 }
 
+/// Fetches an object using the object store and path specified by the encoded `StoragePath`.
+/// This does not need to match the gateway's current object store (e.g. a `StorageKind::Filesystem`)
+/// could be provided even if the gateway is configured with `StorageKind::S3Compatible`.
+/// However, if the provider requires authentication, the gateway must have the correct credentials
+/// set as environment variables.
+///
+/// This is invoked as `GET /internal/object_storage?storage_path=<urlencoded_storagepath>`.
+/// The `<urlencoded_storagepath>` value constructed by serializing `StoragePath` to JSON,
+/// and the urlencoding the resulting string.
+/// For example, `GET /internal/object_storage?storage_path={%22kind%22:{%22type%22:%22filesystem%22,%22path%22:%22/tmp%22},%22path%22:%22fake-tensorzero-file%22}`
 pub async fn get_object_handler(
-    State(AppStateData { .. }): AppState,
+    State(AppStateData { config, .. }): AppState,
     Query(params): Query<PathParams>,
 ) -> Result<Json<ObjectResponse>, Error> {
     // TODO - should we re-use the config object store if it matches?
@@ -32,13 +44,21 @@ pub async fn get_object_handler(
             message: format!("Error parsing storage path: {}", e),
         })
     })?;
-    let store = ObjectStoreInfo::new(Some(storage_path.kind))?.ok_or_else(|| {
-        Error::new(ErrorDetails::InvalidRequest {
-            message: "Could not create ObjectStoreInfo from provided `kind`".to_string(),
-        })
-    })?;
+    // Use the existing object store if it matches the requested kind, so
+    // that we can re-use our connection pool.
+    let store = match &config.object_store_info {
+        Some(store) if store.kind == storage_path.kind => Cow::Borrowed(store),
+        _ => Cow::Owned(
+            ObjectStoreInfo::new(Some(storage_path.kind))?.ok_or_else(|| {
+                Error::new(ErrorDetails::InvalidRequest {
+                    message: "Could not create ObjectStoreInfo from provided `kind`".to_string(),
+                })
+            })?,
+        ),
+    };
     let object = store
         .object_store
+        .as_ref()
         .ok_or_else(|| {
             Error::new(ErrorDetails::InvalidRequest {
                 message: "Object store was disabled".to_string(),
--- tensorzero-internal/tests/e2e/object_storage.rs
+++ tensorzero-internal/tests/e2e/object_storage.rs
@@ -4,7 +4,8 @@ use tensorzero_internal::inference::types::storage::{StorageKind, StoragePath};
 
 use crate::common::get_gateway_endpoint;
 
-// We test a successful fetch in `test_image_inference_with_provider_amazon_s3`
+// We test a successful fetch in `test_image_inference_with_provider_amazon_s3`m
+// so we only need to test an invalid fetch here.
 #[tokio::test]
 async fn test_object_store_fetch_missing() {
     let client = reqwest::Client::new();
