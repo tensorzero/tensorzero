ab22401032e606cd45ee5f1fe5121221a92cada1
maxbokov <maxbokv@gmail.com>
2025-03-12 15:21:55 -0700

    font-display changed to swap

--- ui/tailwind.config.ts
+++ ui/tailwind.config.ts
@@ -37,7 +37,7 @@ export default {
       fontSize: {
         xs: ["0.75rem", { lineHeight: "1rem" }],
         sm: ["0.875rem", { lineHeight: "1.25rem" }],
-        base: ["1rem", { lineHeight: "1.5rem" }],
+        md: ["1rem", { lineHeight: "1.5rem" }],
         lg: ["1.125rem", { lineHeight: "1.75rem" }],
         xl: ["1.25rem", { lineHeight: "1.75rem" }],
         "2xl": ["1.5rem", { lineHeight: "2rem" }],
