23bada567dbed42076912a89aa052201c33718dc
Viraj Mehta <viraj@tensorzero.com>
2025-03-11 21:26:47 -0400

    added llm judge eval for chat function

--- evals/tests/tests.rs
+++ evals/tests/tests.rs
@@ -175,3 +175,98 @@ async fn run_exact_match_eval_chat() {
     }
     assert_eq!(parsed_output.len(), 29);
 }
+
+#[tokio::test(flavor = "multi_thread")]
+async fn run_llm_judge_eval_chat() {
+    let clickhouse = get_clickhouse().await;
+    write_chat_fixture_to_dataset(&PathBuf::from(&format!(
+        "{}/../tensorzero-internal/fixtures/datasets/chat_datapoint_fixture.jsonl",
+        std::env::var("CARGO_MANIFEST_DIR").unwrap()
+    )))
+    .await;
+    let config_path = PathBuf::from(&format!(
+        "{}/../tensorzero-internal/tests/e2e/tensorzero.toml",
+        std::env::var("CARGO_MANIFEST_DIR").unwrap()
+    ));
+    let eval_run_id = Uuid::now_v7();
+    let args = Args {
+        config_file: config_path,
+        gateway_url: None,
+        name: "haiku_without_outputs".to_string(),
+        variant: "gpt_4o_mini".to_string(),
+        concurrency: 10,
+        format: OutputFormat::Jsonl,
+    };
+
+    let mut output = Vec::new();
+    run_eval(args, eval_run_id, &mut output).await.unwrap();
+    clickhouse_flush_async_insert(&clickhouse).await;
+    let output_str = String::from_utf8(output).unwrap();
+    let mut parsed_output = Vec::new();
+    let mut total_prepositions = 0;
+    for line in output_str.lines() {
+        let parsed: EvalInfo = serde_json::from_str(line).expect("Each line should be valid JSON");
+        assert!(parsed.evaluator_errors.is_empty());
+        let inference_id = parsed.response.inference_id();
+        let clickhouse_inference = select_chat_inference_clickhouse(&clickhouse, inference_id)
+            .await
+            .unwrap();
+        let parsed_response = match parsed.response.clone() {
+            InferenceResponse::Chat(chat_response) => chat_response,
+            InferenceResponse::Json(..) => panic!("Json response not supported"),
+        };
+        let clickhouse_input: ResolvedInput =
+            serde_json::from_str(clickhouse_inference["input"].as_str().unwrap()).unwrap();
+        // Check the input to the inference is the same as the input to the datapoint
+        assert_eq!(&clickhouse_input, parsed.datapoint.input());
+        let clickhouse_output: Vec<ContentBlockChatOutput> =
+            serde_json::from_str(clickhouse_inference["output"].as_str().unwrap()).unwrap();
+        // Check the output to the inference is the same as the output in the response
+        assert_eq!(clickhouse_output, parsed_response.content);
+        // Check the inference is properly tagged
+        assert_eq!(
+            clickhouse_inference["tags"]["tensorzero::eval_run_id"],
+            eval_run_id.to_string()
+        );
+
+        // There should be no Boolean feedback for this eval
+        assert!(select_feedback_by_target_id_clickhouse(
+            &clickhouse,
+            "BooleanMetricFeedback",
+            inference_id,
+        )
+        .await
+        .is_none());
+        // The exact match eval should have value None since there is no output in any of these
+        assert!(parsed.evaluations["exact_match"].is_none());
+
+        // There should be Float feedback for this eval
+        let clickhouse_feedback = select_feedback_by_target_id_clickhouse(
+            &clickhouse,
+            "FloatMetricFeedback",
+            inference_id,
+        )
+        .await
+        .unwrap();
+        assert_eq!(
+            clickhouse_feedback["metric_name"].as_str().unwrap(),
+            "tensorzero::eval_name::haiku_without_outputs::evaluator_name::count_prepositions"
+        );
+        assert_eq!(
+            clickhouse_feedback["value"],
+            parsed.evaluations["count_prepositions"]
+                .as_ref()
+                .unwrap()
+                .as_f64()
+                .unwrap()
+        );
+        total_prepositions += clickhouse_feedback["value"].as_f64().unwrap() as u32;
+        assert_eq!(
+            clickhouse_feedback["tags"]["tensorzero::eval_run_id"],
+            eval_run_id.to_string()
+        );
+        parsed_output.push(parsed);
+    }
+    assert_eq!(parsed_output.len(), 23);
+    assert_eq!(total_prepositions, 40);
+}
