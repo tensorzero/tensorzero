e72412340056a36bc2ab54499df47cf3ba289a16
Viraj Mehta <viraj@tensorzero.com>
2025-03-25 18:39:27 -0400

    added playwright test for eval

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
       : "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on",
     video: "on-first-retry",
   },
 
