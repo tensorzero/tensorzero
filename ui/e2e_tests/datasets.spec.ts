24ce2c2790ac6336e55a0fc250f9f7f7299b15d9
Viraj Mehta <viraj@tensorzero.com>
2025-03-25 16:53:09 -0400

    added injected failure to test playwright handling

--- ui/e2e_tests/datasets.spec.ts
+++ ui/e2e_tests/datasets.spec.ts
@@ -3,6 +3,8 @@ import { test, expect } from "@playwright/test";
 test("should show the dataset list page", async ({ page }) => {
   await page.goto("/datasets");
   await expect(page.getByText("Dataset Name")).toBeVisible();
+  // TODO: remove
+  await expect(page.getByText("Dataset Game")).toBeVisible();
 
   // Assert that "error" is not in the page
   await expect(page.getByText("error", { exact: false })).not.toBeVisible();
