8e43351d81909e449648141e25c2c4708515acc3
Viraj Mehta <viraj@tensorzero.com>
2025-03-25 17:12:38 -0400

    added video

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+    video: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
