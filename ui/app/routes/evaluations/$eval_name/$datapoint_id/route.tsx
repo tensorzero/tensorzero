d16f078940b263b1242d9138457bcbc217caf0ba
Viraj Mehta <viraj@tensorzero.com>
2025-03-27 13:58:34 -0400

    wip

--- ui/app/routes/evaluations/$eval_name/$datapoint_id/route.tsx
+++ ui/app/routes/evaluations/$eval_name/$datapoint_id/route.tsx
@@ -1,3 +1,4 @@
+import { getEvalsForDatapoint } from "~/utils/clickhouse/evaluations.server";
 import type { Route } from "./+types/route";
 import { getConfig } from "~/utils/config/index.server";
 
@@ -5,6 +6,23 @@ export async function loader({ request, params }: Route.LoaderArgs) {
   const config = await getConfig();
   const eval_name = params.eval_name;
   const datapoint_id = params.datapoint_id;
+  const url = new URL(request.url);
+  const searchParams = new URLSearchParams(url.search);
+
+  const selected_eval_run_ids = searchParams.get("eval_run_ids");
+  const selected_eval_run_ids_array = selected_eval_run_ids
+    ? selected_eval_run_ids.split(",")
+    : [];
+
+  const evalResults = await getEvalsForDatapoint(
+    eval_name,
+    datapoint_id,
+    selected_eval_run_ids_array,
+  );
+  return {
+    evalResults,
+    selected_eval_run_ids_array,
+  };
 }
 
 export default function EvaluationDatapointPage({
