5d82636155c38333044afc8a93b434346ff88450
Aaron Hill <aaron@tensorzero.com>
2025-04-03 17:09:07 -0400

    Add 'x-tensorzero-gateway-version' header to all gateway responses (#1620)
    We're going to use this to perform feature detection in the client
    code.

--- tensorzero-internal/tests/e2e/health.rs
+++ tensorzero-internal/tests/e2e/health.rs
@@ -1,5 +1,6 @@
 use reqwest::{Client, StatusCode};
 use serde_json::Value;
+use tensorzero_internal::endpoints::status::TENSORZERO_VERSION;
 
 use crate::common::get_gateway_endpoint;
 
@@ -10,6 +11,13 @@ async fn test_health_handler() {
     assert!(response.is_ok());
     let response = response.unwrap();
     assert_eq!(response.status(), StatusCode::OK);
+
+    let version = response
+        .headers()
+        .get("x-tensorzero-gateway-version")
+        .unwrap();
+    assert_eq!(version.to_str().unwrap(), TENSORZERO_VERSION);
+
     let response_value = response.json::<Value>().await;
     assert!(response_value.is_ok());
     let response_value = response_value.unwrap();
