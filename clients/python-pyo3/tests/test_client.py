18c3b60ddd379a6b831b5dd19e6090ebe3842765
Gabriel Bianconi <GabrielBianconi@users.noreply.github.com>
2025-04-06 16:34:23 -0400

    Update

--- clients/python-pyo3/tests/test_client.py
+++ clients/python-pyo3/tests/test_client.py
@@ -1873,6 +1873,8 @@ def test_prepare_inference_request(sync_client: TensorZeroGateway):
         "id": "1",
         "name": "test_tool",
         "arguments": {"arg": "value"},
+        "raw_name": "test_tool",
+        "raw_arguments": '{"arg": "value"}',
     }
     assert request["input"]["messages"][1]["content"][0] == {
         "type": "tool_result",
