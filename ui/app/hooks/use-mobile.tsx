497a3051d17d76e0a629faa3384998be99a3a9c9
Chance Strickland <cs@Chances-MacBook-Pro-2.local>
2025-04-07 22:24:46 -0700

    remove boolean cast

--- ui/app/hooks/use-mobile.tsx
+++ ui/app/hooks/use-mobile.tsx
@@ -3,9 +3,7 @@ import * as React from "react";
 const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
-    undefined,
-  );
+  const [isMobile, setIsMobile] = React.useState(false);
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
@@ -17,5 +15,5 @@ export function useIsMobile() {
     return () => mql.removeEventListener("change", onChange);
   }, []);
 
-  return !!isMobile;
+  return isMobile;
 }
