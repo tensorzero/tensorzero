17f956ce2b58c320f96cfe5c006467bf4ee682c6
Chance Strickland <hi@chance.dev>
2025-04-09 11:18:26 -0700

    Prevent content layout box from growing with children that cause overflow

--- ui/app/components/layout/ContentLayout.tsx
+++ ui/app/components/layout/ContentLayout.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { cn } from "~/utils/common";
-import { ScrollArea } from "~/components/ui/scroll-area";
 import { PageSubNav } from "./PageSubNav";
 
 interface LayoutProviderProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -13,12 +12,12 @@ export function ContentLayout({
   ...props
 }: LayoutProviderProps) {
   return (
-    <div className="bg-bg-tertiary flex-1 py-2 pr-2 pl-0 max-md:p-0">
+    <div className="bg-bg-tertiary w-full min-w-0 flex-1 py-2 pr-2 pl-0 max-md:p-0">
       <div className={cn("h-[calc(100vh-16px)] w-full", className)} {...props}>
-        <ScrollArea className="border-border bg-bg-secondary h-full rounded-md border max-md:rounded-none max-md:border-none">
+        <div className="border-border bg-bg-secondary h-full overflow-hidden rounded-md border max-md:rounded-none max-md:border-none">
           <PageSubNav />
-          {children}
-        </ScrollArea>
+          <div className="h-full overflow-auto">{children}</div>
+        </div>
       </div>
     </div>
   );
