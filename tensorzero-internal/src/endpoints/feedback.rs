a01ddaf333982261ee4783f79e169d0e0becb724
Viraj Mehta <virajmehta@users.noreply.github.com>
2025-04-10 10:24:39 -0400

    Feedback only logs warning if it times out looking for target (#1718)
    * adjusted logging so that the logs for missing target only happen on failure
    
    * added tracing for retry case

--- tensorzero-internal/src/endpoints/feedback.rs
+++ tensorzero-internal/src/endpoints/feedback.rs
@@ -408,7 +408,14 @@ async fn throttled_get_function_name(
             Ok(identifier) => return Ok(identifier),
             Err(err) => {
                 if Instant::now() >= deadline {
+                    // We log here since this means we were not able to find the target_id in the database
+                    // and are timing out.
+                    err.log();
                     return Err(err);
+                } else {
+                    tracing::info!(
+                        "Failed to find function name for target_id: {target_id}. Retrying..."
+                    );
                 }
             }
         }
@@ -457,7 +464,8 @@ async fn get_function_name(
         .trim()
         .to_string();
     if function_name.is_empty() {
-        return Err(Error::new(ErrorDetails::InvalidRequest {
+        // We don't want to log here since this can happen if we send feedback immediately after the target is created.
+        return Err(Error::new_without_logging(ErrorDetails::InvalidRequest {
             message: format!("{identifier_type} ID: {target_id} does not exist"),
         }));
     };
