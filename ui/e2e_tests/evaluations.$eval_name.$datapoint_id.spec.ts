45f3bff49255e7c28b0e2dbc0f0c8ad98f3c02d3
Viraj Mehta <viraj@tensorzero.com>
2025-04-15 10:48:10 -0400

    added playwright tests for evaluation result page

--- ui/e2e_tests/evaluations.$eval_name.$datapoint_id.spec.ts
+++ ui/e2e_tests/evaluations.$eval_name.$datapoint_id.spec.ts
@@ -11,3 +11,62 @@ test("should show the evaluation result page", async ({ page }) => {
   // Assert that "error" is not in the page
   await expect(page.getByText("error", { exact: false })).not.toBeVisible();
 });
+
+test("should be able to add float feedback from the evaluation result page", async ({
+  page,
+}) => {
+  await page.goto(
+    "/evaluations/entity_extraction/0193994e-5560-7610-a3a0-45fdd59338aa?evaluation_run_ids=0196367b-1739-7483-b3f4-f3b0a4bda063%2C0196367b-c0bb-7f90-b651-f90eb9fba8f3",
+  );
+  // Wait for the page to load
+  await page.waitForLoadState("networkidle");
+
+  // Click on the leftmost "pencil" icon
+  await page.locator("svg.lucide-pencil").first().click();
+
+  // Wait for the modal to appear
+  await page.locator('div[role="dialog"]').waitFor({
+    state: "visible",
+  });
+
+  // Fill in the float value
+  // Generate a random float between 0 and 1 with 3 decimal places
+  const randomFloat = Math.floor(Math.random() * 1000) / 1000;
+  await page
+    .getByRole("spinbutton", { name: /Value/i })
+    .fill(randomFloat.toString());
+
+  // Click on the "Save" button
+  await page.locator('button[type="submit"]').click();
+
+  // Assert that the float value is displayed
+  await expect(page.getByText(randomFloat.toString())).toBeVisible();
+});
+
+test("should be able to add bool feedback from the evaluation result page", async ({
+  page,
+}) => {
+  await page.goto(
+    "/evaluations/haiku/01945256-c5b0-7b40-8840-caf0ee0c2c49?evaluation_run_ids=0196367a-702c-75f3-b676-d6ffcc7370a1",
+  );
+
+  // Wait for the page to load
+  await page.waitForLoadState("networkidle");
+
+  // Click on the leftmost "pencil" icon
+  await page.locator("svg.lucide-pencil").first().click();
+
+  // Wait for the modal to appear
+  await page.locator('div[role="dialog"]').waitFor({
+    state: "visible",
+  });
+
+  // Click on the "True" button
+  await page.getByRole("radio", { name: /True/i }).click();
+
+  // Click on the "Save" button
+  await page.locator('button[type="submit"]').click();
+
+  // Assert that the bool value is displayed
+  await expect(page.getByText("True")).toBeVisible();
+});
