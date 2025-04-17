8943178bf9dc2618b485e94713a9359190e26ca7
Aaron Hill <aaron@tensorzero.com>
2025-04-17 12:24:53 -0400

    Verify that we inserted correct number of ui fixture feedback rows (#1847)
    This will help to debug the flaky tests on CI

--- ui/fixtures/load_fixtures.sh
+++ ui/fixtures/load_fixtures.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euxo pipefail
+
 clickhouse-client --host $CLICKHOUSE_HOST --user chuser --password chpassword --database tensorzero_ui_fixtures --query "INSERT INTO JsonInference FORMAT JSONEachRow" < json_inference_examples.jsonl
 clickhouse-client --host $CLICKHOUSE_HOST --user chuser --password chpassword --database tensorzero_ui_fixtures --query "INSERT INTO BooleanMetricFeedback FORMAT JSONEachRow" < boolean_metric_feedback_examples.jsonl
 clickhouse-client --host $CLICKHOUSE_HOST --user chuser --password chpassword --database tensorzero_ui_fixtures --query "INSERT INTO FloatMetricFeedback FORMAT JSONEachRow" < float_metric_feedback_examples.jsonl
@@ -12,8 +14,12 @@ clickhouse-client --host $CLICKHOUSE_HOST --user chuser --password chpassword --
 clickhouse-client --host $CLICKHOUSE_HOST --user chuser --password chpassword --database tensorzero_ui_fixtures --query "INSERT INTO JsonInferenceDatapoint FORMAT JSONEachRow" < json_inference_many_datasets_examples.jsonl
 clickhouse-client --host $CLICKHOUSE_HOST --user chuser --password chpassword --database tensorzero_ui_fixtures --query "INSERT INTO ModelInferenceCache FORMAT JSONEachRow" < model_inference_cache_examples.jsonl
 
+
 # Give ClickHouse some time to make the writes visible
 sleep 1
+
+./check-fixtures.sh
+
 # Create the marker file to signal completion for the healthcheck
 # Write it to an ephemeral location to make sure that we don't see a marker file
 # from a previous container run.
