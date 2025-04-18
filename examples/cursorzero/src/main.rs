2c83d39da261658cf6a373279e28e0b9cdd5f357
Viraj Mehta <viraj@tensorzero.com>
2025-04-18 09:24:20 -0400

    wip on python client

--- examples/cursorzero/src/main.rs
+++ examples/cursorzero/src/main.rs
@@ -1,4 +1,22 @@
+use anyhow::Result;
+use clap::Parser;
+use cursorzero::get_last_commit_from_repo;
+use git2::Repository;
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(short, long, default_value = ".")]
+    path: String,
+}
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    let args = Cli::parse();
+    let repo = Repository::discover(args.path)?;
+    let commit = get_last_commit_from_repo(&repo)?;
+    println!(
+        "Found last commit with message: {}",
+        commit.message().unwrap()
+    );
+
+    Ok(())
 }
