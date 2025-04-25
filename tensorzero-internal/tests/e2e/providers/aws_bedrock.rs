aec816f5df28d1d8c5e43d39f372adbba2e29c77
Oliver Azevedo Barnes <oliver@liquidvoting.io>
2025-04-25 15:05:14 +0100

    Add end to end test for json_mode=off (#1638)
    * Add end to end test for json_mode=off
    
    Closes #568
    
    * Satisfy clippy
    
    * Assert the provider response really isn't json
    
    * New test function for json_mode off, with anthropic variants
    
    * Switch to one variant per provider
    
    * Add one json_mode_off function per provider
    
    * Fix response format checking for openai
    
    * Separate set of providers for json_mode off
    
    * Fix format check
    
    * separate set of e2e test providers for json_mode=off
    
    * Misses
    
    * Check that nothing is parsed as json
    
    * Satisfy deprecation warnings
    
    * Don't try to parse raw output when json mode is off
    
    * Just check for Tokyo to fix flaky test
    
    * Put back CoT
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Add missing supports_batch_inference fields
    
    * fixed cache setting for other human feedback tag
    
    ---------
    
    Co-authored-by: Gabriel Bianconi <GabrielBianconi@users.noreply.github.com>
    Co-authored-by: Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
    Co-authored-by: Viraj Mehta <viraj@tensorzero.com>

--- tensorzero-internal/tests/e2e/providers/aws_bedrock.rs
+++ tensorzero-internal/tests/e2e/providers/aws_bedrock.rs
@@ -59,12 +59,20 @@ async fn get_providers() -> E2ETestProviders {
         },
         E2ETestProvider {
             supports_batch_inference: false,
-            variant_name: "aws-bedrock-default".to_string(),
+            variant_name: "aws-bedrock-strict".to_string(),
             model_name: "claude-3-haiku-20240307-aws-bedrock".into(),
             model_provider_name: "aws_bedrock".into(),
             credentials: HashMap::new(),
         },
     ];
+
+    let json_mode_off_providers = vec![E2ETestProvider {
+        supports_batch_inference: false,
+        variant_name: "aws_bedrock_json_mode_off".to_string(),
+        model_name: "claude-3-haiku-20240307-aws-bedrock".into(),
+        model_provider_name: "aws_bedrock".into(),
+        credentials: HashMap::new(),
+    }];
 
     E2ETestProviders {
         simple_inference: standard_providers.clone(),
@@ -78,6 +86,7 @@ async fn get_providers() -> E2ETestProviders {
         dynamic_tool_use_inference: standard_providers.clone(),
         parallel_tool_use_inference: vec![],
         json_mode_inference: json_providers.clone(),
+        json_mode_off_inference: json_mode_off_providers.clone(),
         image_inference: vec![],
 
         shorthand_inference: vec![],
