550eb3056863b961585f0dae86980c027bfc22ab
Chance Strickland <hi@chance.dev>
2025-04-25 17:54:23 -0700

    types

--- ui/app/components/feedback/HumanFeedbackButton.tsx
+++ ui/app/components/feedback/HumanFeedbackButton.tsx
@@ -1,16 +1,12 @@
 import { Button, type ButtonProps, ButtonIcon } from "~/components/ui/button";
 import { Feedback } from "../icons/Icons";
 
-export interface HumanFeedbackButtonProps {
-  onClick: () => void;
-}
+export type HumanFeedbackButtonProps = Omit<
+  ButtonProps,
+  "children" | "variant" | "size" | "slotLeft" | "slotRight" | "asChild"
+>;
 
-export function HumanFeedbackButton(
-  props: Omit<
-    ButtonProps,
-    "children" | "variant" | "size" | "slotLeft" | "slotRight" | "asChild"
-  >,
-) {
+export function HumanFeedbackButton(props: HumanFeedbackButtonProps) {
   return (
     <Button variant="outline" size="sm" {...props}>
       <ButtonIcon as={Feedback} variant="tertiary" />
