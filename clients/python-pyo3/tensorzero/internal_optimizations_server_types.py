af3d1c11d2c38db243847c84dfb75287a3677414
Gabriel Bianconi <GabrielBianconi@users.noreply.github.com>
2025-05-01 16:37:42 -0400

    Update

--- clients/python-pyo3/tensorzero/internal_optimizations_server_types.py
+++ clients/python-pyo3/tensorzero/internal_optimizations_server_types.py
@@ -4,20 +4,15 @@
 # The types in this file are unstable and subject to change at any time.
 
 
-from typing import Any, List
+from typing import Any
 
-from typing_extensions import NotRequired, Required, TypedDict
+from typing_extensions import Required, TypedDict
 
-from .types import InferenceInput, Message, System
+from .types import InferenceInput
 
 # TODO: we should combine some of these types with the existing types in the client, but we need to make sure they don't trample the existing types
 
 # TODO: we should try to specialize the types of `Any` as much as possible
-
-
-class Input(TypedDict):
-    system: NotRequired[System]
-    messages: NotRequired[List[Message]]
 
 
 class Sample(TypedDict):
