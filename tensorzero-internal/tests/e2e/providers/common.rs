1bd4622f569eac55c2acd914c3e46b2779497e11
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2025-05-05 10:48:42 -0400

    Fix issues with Google AI Studio; make Modal key optional (#2030)
    * Update
    
    * Update
    
    * Update
    
    ---------
    
    Co-authored-by: Gabriel Bianconi <GabrielBianconi@users.noreply.github.com>

--- tensorzero-internal/tests/e2e/providers/common.rs
+++ tensorzero-internal/tests/e2e/providers/common.rs
@@ -2622,6 +2622,7 @@ pub async fn test_simple_streaming_inference_request_with_provider_cache(
     let output = result.get("output").unwrap().as_str().unwrap();
     let output: Vec<ContentBlock> = serde_json::from_str(output).unwrap();
     assert_eq!(output.len(), 1);
+
     // Check the InferenceTag Table
     let result =
         select_inference_tags_clickhouse(&clickhouse, "basic_test", "key", tag_value, inference_id)
@@ -2630,6 +2631,7 @@ pub async fn test_simple_streaming_inference_request_with_provider_cache(
     let id = result.get("inference_id").unwrap().as_str().unwrap();
     let id = Uuid::parse_str(id).unwrap();
     assert_eq!(id, inference_id);
+
     full_content
 }
 
