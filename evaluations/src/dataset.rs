7168692e33990ea741610ce6cb720879692cb649
Viraj Mehta <viraj@tensorzero.com>
2025-05-12 14:39:41 -0400

    wip

--- evaluations/src/dataset.rs
+++ evaluations/src/dataset.rs
@@ -56,5 +56,12 @@ pub async fn query_dataset(
                 .collect()
         }
     };
+    for datapoint in datapoints.iter() {
+        #[expect(clippy::unwrap_used)]
+        let input_string = serde_json::to_string_pretty(datapoint.input()).unwrap();
+        if input_string.contains("Mauricio") {
+            println!("datapoint: {:?}", datapoint);
+        }
+    }
     Ok(datapoints)
 }
