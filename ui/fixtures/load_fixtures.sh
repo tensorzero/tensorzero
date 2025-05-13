3a44d2363b037b7606b45515caffc9a37568f3b8
Aaron Hill <aaron@tensorzero.com>
2025-05-13 13:28:28 -0400

    Show disks

--- ui/fixtures/load_fixtures.sh
+++ ui/fixtures/load_fixtures.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
+clickhouse-client --user chuser --password chpassword "SELECT * FROM system.disks;"
+
 clickhouse-client --host $CLICKHOUSE_HOST --user chuser --password chpassword --database tensorzero_ui_fixtures --query "INSERT INTO JsonInference FORMAT JSONEachRow" < json_inference_examples.jsonl
 clickhouse-client --host $CLICKHOUSE_HOST --user chuser --password chpassword --database tensorzero_ui_fixtures --query "INSERT INTO BooleanMetricFeedback FORMAT JSONEachRow" < boolean_metric_feedback_examples.jsonl
 clickhouse-client --host $CLICKHOUSE_HOST --user chuser --password chpassword --database tensorzero_ui_fixtures --query "INSERT INTO FloatMetricFeedback FORMAT JSONEachRow" < float_metric_feedback_examples.jsonl
