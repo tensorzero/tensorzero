5df0ab748998541ced70ce75c207ba7531458b0b
Viraj Mehta <viraj@tensorzero.com>
2025-05-20 12:14:49 -0400

    fix dicl notebook

--- recipes/dicl/dicl_nb.py
+++ recipes/dicl/dicl_nb.py
@@ -1,18 +1,4 @@
-# %%
 # type: ignore
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.17.1
-#   kernelspec:
-#     display_name: .venv
-#     language: python
-#     name: python3
-# ---
 
 # %% [markdown]
 # # Dynamic In-Context Learning
