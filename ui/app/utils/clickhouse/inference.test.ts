aad972c2009a42c9e43b360cfe8d9a0c93ea3e03
Aaron Hill <aaron@tensorzero.com>
2025-05-28 12:02:31 -0400

    Fix another test

--- ui/app/utils/clickhouse/inference.test.ts
+++ ui/app/utils/clickhouse/inference.test.ts
@@ -599,7 +599,7 @@ describe("getAdjacentInferenceIds", () => {
       lastInferenceId.data[0].last_inference_id,
     );
     expect(adjacentInferenceIds.previous_id).toBe(
-      "0196eea6-42cb-7bf1-a56a-51d68ab08f60",
+      "0197177a-7c00-70a2-82a6-72ac87d2ff77",
     );
     expect(adjacentInferenceIds.next_id).toBeNull();
   });
