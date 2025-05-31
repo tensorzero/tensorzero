6a3bb67ef958337fb011509989a2bf1bc1e3d4b9
Viraj Mehta <viraj@tensorzero.com>
2025-05-31 12:07:02 -0400

    wip

---
+++ tensorzero-internal/src/clickhouse/query_builder/mod.rs
@@ -0,0 +1,52 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum InferenceOutputSource {
+    Inference,
+    Demonstration,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum FloatComparisonOperator {
+    LessThan,
+    LessThanOrEqual,
+    Equal,
+    GreaterThan,
+    GreaterThanOrEqual,
+    NotEqual,
+}
+
+impl FloatComparisonOperator {
+    pub fn to_clickhouse_operator(&self) -> &str {
+        match self {
+            FloatComparisonOperator::LessThan => "<",
+            FloatComparisonOperator::LessThanOrEqual => "<=",
+            FloatComparisonOperator::Equal => "=",
+            FloatComparisonOperator::GreaterThan => ">",
+            FloatComparisonOperator::GreaterThanOrEqual => ">=",
+            FloatComparisonOperator::NotEqual => "!=",
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct FloatMetricNode {
+    pub metric_name: String,
+    pub value: f64,
+    pub comparison_operator: FloatComparisonOperator,
+}
+
+#[derive(Debug, Clone)]
+pub struct BooleanMetricNode {
+    pub metric_name: String,
+    pub value: bool,
+}
+
+#[derive(Debug, Clone)]
+pub enum FilterOperatorNode {
+    Float(FloatMetricNode),
+    Boolean(BooleanMetricNode),
+}
+
+#[derive(Debug, Clone)]
+pub struct FilterNode {
+    pub operator: FilterOperatorNode,
+}
