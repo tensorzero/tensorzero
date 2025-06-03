408c79f88a8de0f6aa0bc2ed00487b834a25b892
Viraj Mehta <viraj@tensorzero.com>
2025-06-03 16:51:10 -0400

    more integration tests

--- tensorzero-internal/tests/e2e/list_inferences.rs
+++ tensorzero-internal/tests/e2e/list_inferences.rs
@@ -1,6 +1,6 @@
 use tensorzero::{
-    FloatComparisonOperator, FloatMetricNode, InferenceFilterTreeNode, InferenceOutputSource,
-    ListInferencesParams, StoredInference,
+    BooleanMetricNode, FloatComparisonOperator, FloatMetricNode, InferenceFilterTreeNode,
+    InferenceOutputSource, ListInferencesParams, StoredInference,
 };
 use tensorzero_internal::clickhouse::ClickhouseFormat;
 
@@ -69,6 +69,55 @@ pub async fn test_simple_query_with_float_filter() {
     };
     let res = client.experimental_list_inferences(opts).await.unwrap();
     assert_eq!(res.len(), 1);
+    for inference in res {
+        let StoredInference::Json(json_inference) = inference else {
+            panic!("Expected a JSON inference");
+        };
+        assert_eq!(json_inference.function_name, "extract_entities");
+    }
+}
+
+#[tokio::test(flavor = "multi_thread")]
+pub async fn test_demonstration_output_source() {
+    let client = make_embedded_gateway().await;
+    let opts = ListInferencesParams {
+        function_name: "extract_entities",
+        variant_name: None,
+        filters: None,
+        output_source: InferenceOutputSource::Demonstration,
+        limit: Some(5),
+        offset: Some(1),
+        format: ClickhouseFormat::JsonEachRow,
+    };
+
+    let res = client.experimental_list_inferences(opts).await.unwrap();
+    assert_eq!(res.len(), 5);
+    for inference in res {
+        let StoredInference::Json(json_inference) = inference else {
+            panic!("Expected a JSON inference");
+        };
+        assert_eq!(json_inference.function_name, "extract_entities");
+    }
+}
+
+#[tokio::test(flavor = "multi_thread")]
+pub async fn test_boolean_metric_filter() {
+    let client = make_embedded_gateway().await;
+    let filter_node = InferenceFilterTreeNode::BooleanMetric(BooleanMetricNode {
+        metric_name: "exact_match".to_string(),
+        value: true,
+    });
+    let opts = ListInferencesParams {
+        function_name: "extract_entities",
+        variant_name: None,
+        filters: Some(&filter_node),
+        output_source: InferenceOutputSource::Inference,
+        limit: Some(5),
+        offset: Some(1),
+        format: ClickhouseFormat::JsonEachRow,
+    };
+    let res = client.experimental_list_inferences(opts).await.unwrap();
+    assert_eq!(res.len(), 5);
     for inference in res {
         let StoredInference::Json(json_inference) = inference else {
             panic!("Expected a JSON inference");
