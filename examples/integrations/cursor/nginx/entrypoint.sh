7e74a3d0ff3c3f5db8012d17388e0ad12862318a
Viraj Mehta <viraj@tensorzero.com>
2025-06-08 18:29:58 -0400

    first attempt at user tag

--- examples/integrations/cursor/nginx/entrypoint.sh
+++ examples/integrations/cursor/nginx/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
-# Replace environment variables in the template
-envsubst '${API_TOKEN}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+# Replace environment variables in the template (USER is optional)
+envsubst '${API_TOKEN} ${USER}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+# Copy the Lua script to the nginx directory
+cp /modify_body.lua /etc/nginx/modify_body.lua
 
 # Execute the default nginx command
 exec "$@"
