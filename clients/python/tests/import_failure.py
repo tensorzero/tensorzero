0b73ce279e8e1b1c42148af086bdf7c4476d1d8a
Viraj Mehta <viraj@tensorzero.com>
2025-06-13 17:41:22 -0400

    fixed pyright ignore statement

--- clients/python/tests/import_failure.py
+++ clients/python/tests/import_failure.py
@@ -6,6 +6,8 @@ import os
 os.environ["RUST_LOG"] = "foo=bar"
 
 try:
+    import tensorzero  # noqa # pyright: ignore[reportUnusedImport]
+
     raise Exception("TensorZero import succeeded - this should not happen")
 except Exception as e:
     assert (
