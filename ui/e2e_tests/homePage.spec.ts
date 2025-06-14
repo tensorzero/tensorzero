7e8d0f71341e2cfc7976db31bf4172b674b8db61
Max Bokov <maxbokov@users.noreply.github.com>
2025-06-14 07:24:32 -0700

    Update dashboard layout (#2449)
    * updated dashboard layout
    
    * fixed requirements file
    
    * fixed e2e test issue
    
    * Fix dependencies: restore ui/fixtures/requirements.txt from main and update all requirements.txt files
    
    * Re-freeze
    
    ---------
    
    Co-authored-by: Viraj Mehta <viraj@tensorzero.com>
    Co-authored-by: Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>

--- ui/e2e_tests/homePage.spec.ts
+++ ui/e2e_tests/homePage.spec.ts
@@ -2,11 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("should show the home page", async ({ page }) => {
   await page.goto("/");
-  await expect(
-    page.getByText(
-      "Monitor metrics across models and prompts and debug individual API calls",
-    ),
-  ).toBeVisible();
+  await expect(page.getByText("Ask a question")).toBeVisible();
 
   // Assert that "error" is not in the page
   await expect(page.getByText("error", { exact: false })).not.toBeVisible();
