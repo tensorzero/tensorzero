0aca90458890c2a887536e2356b781cd04a4eb7c
Aaron Hill <aaron@tensorzero.com>
2025-06-25 13:10:06 -0400

    Use secret and redact clickhouse url

--- ci/buildkite/test-clickhouse-cloud.sh
+++ ci/buildkite/test-clickhouse-cloud.sh
@@ -4,7 +4,10 @@ echo "CLICKHOUSE_ID=${CLICKHOUSE_ID}"
 # Get the url of a ClickHouse cloud instance, based on the $CLICKHOUSE_ID that we assigned in '.buildkite/hooks/post-checkout'
 # We also use this id as part of the concurrent group key in '.buildkite/pipeline.yml', so we're guaranteed to be the only job trying to use this instance.
 # This allows us to distribute our tests across multiple ClickHouse cloud instances (since running too many parallel tests will overload any single instance).
+export CLICKHOUSE_API_KEY=$(buildkite-agent secret get clickhouse_api_key)
+export CLICKHOUSE_KEY_SECRET=$(buildkite-agent secret get clickhouse_key_secret)
 export TENSORZERO_CLICKHOUSE_SERVICE=$(curl --user "$CLICKHOUSE_API_KEY:$CLICKHOUSE_KEY_SECRET" https://api.clickhouse.cloud/v1/organizations/b55f1935-803f-4931-90b3-4d26089004d4/services | jq ".result[] | select(.name == \"dev-tensorzero-e2e-tests-instance-${CLICKHOUSE_ID}\") | .id")
+echo $TENSORZERO_CLICKHOUSE_URL | buildkite-agent redactor add
 curl "$TENSORZERO_CLICKHOUSE_URL" --data-binary 'SHOW DATABASES'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
 . "$HOME/.cargo/env"
