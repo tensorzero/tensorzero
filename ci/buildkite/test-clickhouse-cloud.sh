563150e4aad250eaf4bd78e5a6e7f948c2f3f546
Aaron Hill <aaron@tensorzero.com>
2025-06-25 13:26:43 -0400

    Redact url

--- ci/buildkite/test-clickhouse-cloud.sh
+++ ci/buildkite/test-clickhouse-cloud.sh
@@ -8,7 +8,12 @@ export CLICKHOUSE_API_KEY=$(buildkite-agent secret get clickhouse_api_key)
 export CLICKHOUSE_KEY_SECRET=$(buildkite-agent secret get clickhouse_key_secret)
 export CLICKHOUSE_USERNAME=$(buildkite-agent secret get clickhouse_username)
 export CLICKHOUSE_PASSWORD=$(buildkite-agent secret get clickhouse_password)
+
+set +x
 export TENSORZERO_CLICKHOUSE_URL=$(curl --user "$CLICKHOUSE_API_KEY:$CLICKHOUSE_KEY_SECRET" https://api.clickhouse.cloud/v1/organizations/b55f1935-803f-4931-90b3-4d26089004d4/services | jq -r ".result[] | select(.name == \"dev-tensorzero-e2e-tests-instance-${CLICKHOUSE_ID}\") | .endpoints[] | select(.protocol == \"https\") | \"https://$CLICKHOUSE_USERNAME:$CLICKHOUSE_PASSWORD@\" + .host + \":\" + (.port | tostring)")
+echo $TENSORZERO_CLICKHOUSE_URL | buildkite-agent redactor add
+set -x
+
 curl "$TENSORZERO_CLICKHOUSE_URL" --data-binary 'SHOW DATABASES'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
 . "$HOME/.cargo/env"
