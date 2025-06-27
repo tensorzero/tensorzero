73c54a25d4d9582bb6c44ce00a6083518d7678f6
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2025-06-27 09:32:01 -0400

    Hide breadcrumbs on homepage

--- ui/app/types/breadcrumbs.ts
+++ ui/app/types/breadcrumbs.ts
@@ -2,7 +2,9 @@ import type { UIMatch } from "react-router";
 
 declare module "react-router" {
   interface RouteHandle {
-    /** Append 0, 1 or more breadcrumbs given current segment */
+    /** Should it hide breadcrumbs for this route? */
+    hideBreadcrumbs?: boolean;
+    /** Append 0, 1 or more breadcrumbs given current segment. Used to hierarchically generate page title. */
     crumb?: (match: UIMatch) => string[];
   }
 }
