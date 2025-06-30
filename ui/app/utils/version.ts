e0cd9e46dbef6ddee0f582b90f3eb1311361b74f
Chetan <chetanbaliyan10@gmail.com>
2025-07-01 03:51:22 +0530

    feat: add ui version logging (#2569)
    * feat: add ui version logging
    
    * moved version to package
    
    * oops put back version file
    
    * fix: use __APP_VERSION__ as global instead of module export
    
    * fixed assertion for logging
    
    * removed version file
    
    ---------
    
    Co-authored-by: Viraj Mehta <viraj@tensorzero.com>

--- ui/app/utils/version.ts
+++
@@ -1,8 +0,0 @@
-/**
- * TensorZero UI version information
- *
- * This module exports the current version of the TensorZero UI.
- * It serves as the single source of truth for version information.
- */
-
-export const VERSION = "2025.6.3";
