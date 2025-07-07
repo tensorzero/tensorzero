6138dcc785144a99d0378e5484a51e637fd64759
Viraj Mehta <viraj@tensorzero.com>
2025-07-07 10:22:10 -0400

    wip

--- tensorzero-core/src/optimization/openai_sft/mod.rs
+++ tensorzero-core/src/optimization/openai_sft/mod.rs
@@ -76,51 +76,37 @@ impl UninitializedOpenAISFTConfig {
     /// ($self, /, *args, **kwargs)
     /// Same is true for FireworksSFTConfig
     #[expect(clippy::too_many_arguments)]
-    // #[new]
-    // #[pyo3(signature = (*, model="openai/gpt-4o-mini".to_string(), batch_size=None, learning_rate_multiplier=None, n_epochs=None, credentials=None, api_base=None, seed=None, suffix=None))]
+    #[new]
+    #[pyo3(signature = (*, model, batch_size=None, learning_rate_multiplier=None, n_epochs=None, credentials=None, api_base=None, seed=None, suffix=None))]
     // #[pyo3(signature = (base_url, *, timeout=None))]
     pub fn new(
-        py: Python<'_>,
-        base_url: &str,
-        timeout: Option<f64>,
-        // model: String,
-        // batch_size: Option<usize>,
-        // learning_rate_multiplier: Option<f64>,
-        // n_epochs: Option<usize>,
-        // credentials: Option<String>,
-        // api_base: Option<String>,
-        // seed: Option<u64>,
-        // suffix: Option<String>,
+        model: String,
+        batch_size: Option<usize>,
+        learning_rate_multiplier: Option<f64>,
+        n_epochs: Option<usize>,
+        credentials: Option<String>,
+        api_base: Option<String>,
+        seed: Option<u64>,
+        suffix: Option<String>,
     ) -> PyResult<Self> {
         // Use Deserialize to convert the string to a CredentialLocation
-        panic!("Stop here");
-        // let credentials =
-        //     credentials.map(|s| serde_json::from_str(&s).unwrap_or(CredentialLocation::Env(s)));
-        // let api_base = api_base
-        //     .map(|s| {
-        //         Url::parse(&s)
-        //             .map_err(|e| PyErr::new::<PyValueError, std::string::String>(e.to_string()))
-        //     })
-        //     .transpose()?;
-        // Ok(Self {
-        //     model,
-        //     batch_size,
-        //     learning_rate_multiplier,
-        //     n_epochs,
-        //     credentials,
-        //     api_base,
-        //     seed,
-        //     suffix,
-        // })
+        let credentials =
+            credentials.map(|s| serde_json::from_str(&s).unwrap_or(CredentialLocation::Env(s)));
+        let api_base = api_base
+            .map(|s| {
+                Url::parse(&s)
+                    .map_err(|e| PyErr::new::<PyValueError, std::string::String>(e.to_string()))
+            })
+            .transpose()?;
         Ok(Self {
-            model: "openai/gpt-4o-mini".to_string(),
-            batch_size: None,
-            learning_rate_multiplier: None,
-            n_epochs: None,
-            credentials: None,
-            api_base: None,
-            seed: None,
-            suffix: None,
+            model,
+            batch_size,
+            learning_rate_multiplier,
+            n_epochs,
+            credentials,
+            api_base,
+            seed,
+            suffix,
         })
     }
 }
