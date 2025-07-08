210aa7bc99fc069425bb05942d9697c8d7866204
Kincaid O'Neil <kincaidoneil@users.noreply.github.com>
2025-07-08 18:54:34 -0400

    Revert BasicInfoLayout changes

--- ui/app/components/layout/BasicInfoLayout.tsx
+++ ui/app/components/layout/BasicInfoLayout.tsx
@@ -1,56 +1,23 @@
-import { cn } from "~/utils/common";
+export function BasicInfoLayout({ children }: React.PropsWithChildren) {
+  return <div className="flex flex-col gap-4 md:gap-2">{children}</div>;
+}
 
-export const BasicInfoLayout: React.FC<React.ComponentProps<"div">> = ({
-  children,
-  className,
-  ...props
-}) => (
-  <div className={cn("flex flex-col gap-4 md:gap-2", className)} {...props}>
-    {children}
-  </div>
-);
+export function BasicInfoItem({ children }: React.PropsWithChildren) {
+  return <div className="flex flex-col gap-0.5 md:flex-row">{children}</div>;
+}
 
-export const BasicInfoItem: React.FC<React.ComponentProps<"div">> = ({
-  children,
-  className,
-  ...props
-}) => (
-  <div
-    className={cn("flex flex-col gap-0.5 md:flex-row", className)}
-    {...props}
-  >
-    {children}
-  </div>
-);
+export function BasicInfoItemTitle({ children }: React.PropsWithChildren) {
+  return (
+    <div className="text-fg-secondary w-full flex-shrink-0 text-left text-sm md:w-32 md:py-1">
+      {children}
+    </div>
+  );
+}
 
-export const BasicInfoItemTitle: React.FC<React.ComponentProps<"div">> = ({
-  children,
-  className,
-  ...props
-}) => (
-  <div
-    className={cn(
-      "text-fg-secondary w-full flex-shrink-0 text-left text-sm md:w-32 md:py-1",
-      className,
-    )}
-    {...props}
-  >
-    {children}
-  </div>
-);
-
-export const BasicInfoItemContent: React.FC<React.ComponentProps<"div">> = ({
-  children,
-  className,
-  ...props
-}) => (
-  <div
-    className={cn(
-      "text-fg-primary flex flex-wrap gap-x-4 gap-y-0.5 truncate md:gap-1 md:py-1",
-      className,
-    )}
-    {...props}
-  >
-    {children}
-  </div>
-);
+export function BasicInfoItemContent({ children }: React.PropsWithChildren) {
+  return (
+    <div className="text-fg-primary flex flex-wrap gap-x-4 gap-y-0.5 truncate md:gap-1 md:py-1">
+      {children}
+    </div>
+  );
+}
