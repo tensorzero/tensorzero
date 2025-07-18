bc177dc2179598c014f904690b79df914509d00a
Viraj Mehta <viraj@tensorzero.com>
2025-07-18 10:37:36 -0400

    added updated dumping code

--- ui/app/utils/config/models.ts
+++ ui/app/utils/config/models.ts
@@ -1,46 +1,32 @@
 import { stringify } from "smol-toml";
-import type { OptimizerOutput, ProviderConfig } from "tensorzero-node";
-
-type ProviderType = ProviderConfig["type"];
-
-function createProviderConfig(
-  type: ProviderType,
-  model_name: string,
-): ProviderConfig {
-  switch (type) {
-    case "fireworks":
-      return { type, model_name, parse_think_blocks: false };
-    case "openai":
-      return { type, model_name, api_base: null };
-    default:
-      throw new Error(`Provider ${type} requires additional configuration`);
-  }
-}
-
-export function get_fine_tuned_provider_config(
-  model_name: string,
-  model_provider_type: ProviderType,
-) {
-  const providerConfig = createProviderConfig(model_provider_type, model_name);
-  return providerConfig;
-}
+import type { OptimizerOutput } from "tensorzero-node";
 
 export function dump_optimizer_output(optimizerOutput: OptimizerOutput) {
-  return stringify(optimizerOutput);
-  // const fullyQualifiedProviderConfig = {
-  //   models: {
-  //     [modelName]: {
-  //       routing: [modelName],
-  //       providers: {
-  //         [modelName]: providerConfig,
-  //       },
-  //     },
-  //   },
-  // };
-  // const rawSerializedModelConfig = stringify(fullyQualifiedProviderConfig);
-  // const lines = rawSerializedModelConfig.split("\n");
-  // const linesWithoutFirst = lines.slice(1);
-  // linesWithoutFirst.splice(3, 1);
-  // const trimmedSerializedModelConfig = linesWithoutFirst.join("\n");
-  // return trimmedSerializedModelConfig;
+  /// Drop type key from the optimizer output
+  const { type, ...rest } = optimizerOutput;
+  if (type !== "model") {
+    throw new Error(`Only model type is supported, got ${type}`);
+  }
+  if (rest.routing.length !== 1) {
+    throw new Error(`Expected 1 routing entry, got ${rest.routing.length}`);
+  }
+  const modelName = rest.routing[0];
+  const providerConfig = rest.providers[modelName];
+  console.log(JSON.stringify(providerConfig, null, 2));
+  const fullyQualifiedProviderConfig = {
+    models: {
+      [modelName]: {
+        routing: [modelName],
+        providers: {
+          [modelName]: providerConfig,
+        },
+      },
+    },
+  };
+  const rawSerializedModelConfig = stringify(fullyQualifiedProviderConfig);
+  const lines = rawSerializedModelConfig.split("\n");
+  const linesWithoutFirst = lines.slice(1);
+  linesWithoutFirst.splice(3, 1);
+  const trimmedSerializedModelConfig = linesWithoutFirst.join("\n");
+  return trimmedSerializedModelConfig;
 }
