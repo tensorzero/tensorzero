0249d4917406ec56b2e1cb15c7dd8fe9f4eb103f
Viraj Mehta <viraj@tensorzero.com>
2025-08-19 12:05:55 -0400

    fixed migration manager test

--- tensorzero-core/tests/e2e/clickhouse.rs
+++ tensorzero-core/tests/e2e/clickhouse.rs
@@ -727,7 +727,7 @@ async fn test_clickhouse_migration_manager() {
         // for each element in the array.
         [
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-            24, 25, 26, 27
+            24, 25, 26, 27, 28
         ]
     );
     let rows = get_all_migration_records(&clickhouse).await.unwrap();
