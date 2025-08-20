90e5ca1669ee11426217fb8238a4c0123a0e7e0d
Gabriel Bianconi <GabrielBianconi@users.noreply.github.com>
2025-08-20 19:17:50 -0400

    ...

--- ci/buildkite/buildkite-exploration.sh
+++ ci/buildkite/buildkite-exploration.sh
@@ -1,12 +1,8 @@
 # Install cargo-binstall
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
-# Install uv
-curl -LsSf https://astral.sh/uv/0.6.17/install.sh | sh
-export PATH=$PATH:$HOME/.local/bin
-
 # Install Python
-uv python install 3.13 --default --preview
+apt update && apt install -y python3.13
 
 # Install cargo-nextest
 # TODO: install the pre-built package instead of building from source
