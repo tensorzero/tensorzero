6559e2f6fd0291d831b82735ef9124e0ba8c4cee
Gabriel Bianconi <GabrielBianconi@users.noreply.github.com>
2025-08-20 19:22:18 -0400

    ...

--- ci/buildkite/buildkite-exploration.sh
+++ ci/buildkite/buildkite-exploration.sh
@@ -2,7 +2,7 @@
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 # Install Python
-apt update && apt install -y python3.13
+apt update && apt install -y python3.13-dev
 
 # Install cargo-nextest
 # TODO: install the pre-built package instead of building from source
