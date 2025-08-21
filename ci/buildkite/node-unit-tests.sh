13245864a4aa471bd47dfdaf6d9472b37b469177
Viraj Mehta <viraj@tensorzero.com>
2025-08-21 17:02:53 -0400

    try fvm instead of nvm

--- ci/buildkite/node-unit-tests.sh
+++ ci/buildkite/node-unit-tests.sh
@@ -16,15 +16,15 @@ source $HOME/.cargo/env
 rustc --version
 
 # Install Node
-# Download and install nvm:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-# in lieu of restarting the shell
-\. "$HOME/.nvm/nvm.sh"
+# Download and install fnm:
+curl -o- https://fnm.vercel.app/install | bash
+
 # Download and install Node.js:
-nvm install 22
+fnm install 22
+
 # Verify the Node.js version:
 node -v # Should print "v22.18.0".
-nvm current # Should print "v22.18.0".
+
 # Verify npm version:
 npm -v # Should print "10.9.3".
 
