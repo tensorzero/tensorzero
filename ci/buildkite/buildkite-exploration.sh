e5dcab24571988370a7029ea9a71bfb9a70026f4
Gabriel Bianconi <GabrielBianconi@users.noreply.github.com>
2025-08-20 20:05:52 -0400

    ...

--- ci/buildkite/buildkite-exploration.sh
+++ ci/buildkite/buildkite-exploration.sh
@@ -1,12 +1,12 @@
 # Set BUILDKITE_ANALYTICS_TOKEN
-echo $BUILDKITE_ANALYTICS_TOKEN
-echo "AAA"
-export BUILDKITE_ANALYTICS_TOKEN=$(buildkite-agent secret get CI_UNIT_BUILDKITE_ANALYTICS_TOKEN)
-echo $BUILDKITE_ANALYTICS_TOKEN
-echo "XXX"
-echo $(buildkite-agent secret get CI_UNIT_BUILDKITE_ANALYTICS_TOKEN)
-echo "YYY"
-exit 1
+# echo $BUILDKITE_ANALYTICS_TOKEN
+# echo "AAA"
+# export BUILDKITE_ANALYTICS_TOKEN=$(buildkite-agent secret get CI_UNIT_BUILDKITE_ANALYTICS_TOKEN)
+# echo $BUILDKITE_ANALYTICS_TOKEN
+# echo "XXX"
+# echo $(buildkite-agent secret get CI_UNIT_BUILDKITE_ANALYTICS_TOKEN)
+# echo "YYY"
+# exit 1
 
 # Install cargo-binstall
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
