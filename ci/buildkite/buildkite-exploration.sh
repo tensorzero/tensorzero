1636d4cc63557d549aaf912cccb1683df2a770ae
Gabriel Bianconi <GabrielBianconi@users.noreply.github.com>
2025-08-20 20:12:53 -0400

    ...

--- ci/buildkite/buildkite-exploration.sh
+++ ci/buildkite/buildkite-exploration.sh
@@ -1,9 +1,6 @@
 # Set BUILDKITE_ANALYTICS_TOKEN
-export BUILDKITE_ANALYTICS_TOKEN=$(buildkite-agent secret get CI_UNIT_BUILDKITE_ANALYTICS_TOKN)
-
 export BUILDKITE_ANALYTICS_TOKEN=$(buildkite-agent secret get CI_UNIT_BUILDKITE_ANALYTICS_TOKEN)
 
-# Check if BUILDKITE_ANALYTICS_TOKEN is set
 if [ -z "$BUILDKITE_ANALYTICS_TOKEN" ]; then
     echo "Error: BUILDKITE_ANALYTICS_TOKEN is not set"
     exit 1
@@ -24,3 +21,18 @@ cargo install buildkite-test-collector
 
 # Run unit tests using cargo-nextest
 cargo test-unit --profile ci-unit
+
+# Upload the test JUnit XML files
+curl -X POST \
+  -H "Authorization: Token token=$BUILDKITE_ANALYTICS_TOKEN" \
+  -F "format=junit" \
+  -F "data=@target/nextest/ci-unit/junit.xml" \
+  -F "run_env[CI]=buildkite" \
+  -F "run_env[key]=$BUILDKITE_BUILD_ID" \
+  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
+  -F "run_env[branch]=$BUILDKITE_BRANCH" \
+  -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
+  -F "run_env[message]=$BUILDKITE_MESSAGE" \
+  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
+  https://analytics-api.buildkite.com/v1/uploads
