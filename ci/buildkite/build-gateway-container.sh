a77046f833463f1bf7138e294b8daa841e11a37e
Viraj Mehta <viraj@tensorzero.com>
2025-08-21 16:02:32 -0400

    added a load

--- ci/buildkite/build-gateway-container.sh
+++ ci/buildkite/build-gateway-container.sh
@@ -5,5 +5,5 @@ set -euxo pipefail
 # Get the short hash from the buildkite environment variable
 SHORT_HASH=${BUILDKITE_COMMIT:0:7}
 # Build and save container
-docker build --build-arg BUILDKIT_CONTEXT_KEEP_GIT_DIR=1 -f gateway/Dockerfile . -t tensorzero/gateway:sha-$SHORT_HASH
+docker build --load --build-arg BUILDKIT_CONTEXT_KEEP_GIT_DIR=1 -f gateway/Dockerfile . -t tensorzero/gateway:sha-$SHORT_HASH
 docker save tensorzero/gateway:sha-$SHORT_HASH > gateway-container.tar
