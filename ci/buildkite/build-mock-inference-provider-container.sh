a0f722b79f7ad1f73dd6eb2381be3ac529c4b517
Viraj Mehta <viraj@tensorzero.com>
2025-08-25 19:14:58 -0400

    added a dependency on the mock inference provider

--- ci/buildkite/build-mock-inference-provider-container.sh
+++ ci/buildkite/build-mock-inference-provider-container.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
-
-set -euxo pipefail
+#!/bin/bash
+set -euo pipefail
 
 # Get the short hash from the buildkite environment variable
 SHORT_HASH=${BUILDKITE_COMMIT:0:7}
-# Build and save container
-docker build --load -f tensorzero-core/tests/mock-inference-provider/Dockerfile . -t tensorzero/mock-inference-provider:sha-$SHORT_HASH
-docker save tensorzero/mock-inference-provider:sha-$SHORT_HASH > mock-inference-provider-container.tar
+
+# Build the container
+docker build --load --build-arg BUILDKIT_CONTEXT_KEEP_GIT_DIR=1 -f tensorzero-core/tests/mock-inference-provider/Dockerfile . -t tensorzero/mock-inference-provider:ci-sha-$SHORT_HASH
+
+source ci/buildkite/utils/docker-hub-credentials.sh
+
+# Login to Docker Hub (make sure DOCKER_HUB_USERNAME and DOCKER_HUB_ACCESS_TOKEN are set)
+echo "$DOCKER_HUB_ACCESS_TOKEN" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
+
+# Push to Docker Hub
+docker push tensorzero/mock-inference-provider:ci-sha-$SHORT_HASH
