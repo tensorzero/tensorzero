585cb0fbbaad31a5a6f4b7d8bfe87d3703d96691
Viraj Mehta <viraj@tensorzero.com>
2025-08-25 19:58:43 -0400

    added credentials to ui

--- ci/buildkite/build-ui-container.sh
+++ ci/buildkite/build-ui-container.sh
@@ -6,6 +6,8 @@ SHORT_HASH=${BUILDKITE_COMMIT:0:7}
 # Build container
 docker build --load --build-arg BUILDKIT_CONTEXT_KEEP_GIT_DIR=1 -f ui/Dockerfile . -t tensorzero/ui-dev:ci-sha-$SHORT_HASH
 
+source ci/buildkite/utils/docker-hub-credentials.sh
+
 # Login to Docker Hub (make sure DOCKER_HUB_USERNAME and DOCKER_HUB_ACCESS_TOKEN are set)
 echo "$DOCKER_HUB_ACCESS_TOKEN" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
 
