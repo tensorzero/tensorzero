def7c56f719bcd26ec3e332a2548e17881eb1aa0
Viraj Mehta <viraj@tensorzero.com>
2025-08-25 19:57:38 -0400

    dropped a file

---
+++ ci/buildkite/build-ui-e2e-tests-container.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -euo pipefail
+
+# Get the short hash from the buildkite environment variable
+SHORT_HASH=${BUILDKITE_COMMIT:0:7}
+
+# Build container
+docker build --load --build-arg BUILDKIT_CONTEXT_KEEP_GIT_DIR=1 -f ui/fixtures/Dockerfile.e2e . -t tensorzero/e2e-tests:ci-sha-$SHORT_HASH
+
+# Login to Docker Hub (make sure DOCKER_HUB_USERNAME and DOCKER_HUB_ACCESS_TOKEN are set)
+echo "$DOCKER_HUB_ACCESS_TOKEN" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
+
+# Push to Docker Hub
+docker push tensorzero/e2e-tests:ci-sha-$SHORT_HASH
