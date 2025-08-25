390e64cd7906010fd8083cbe7b605b4815a5ae47
Viraj Mehta <viraj@tensorzero.com>
2025-08-25 16:34:37 -0400

    try including node unit test analytics

--- ci/buildkite/node-unit-tests.sh
+++ ci/buildkite/node-unit-tests.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
+
+# Set up Buildkite test analytics collection
+export BUILDKITE_ANALYTICS_TOKEN=$(buildkite-agent secret get NODE_UNIT_ANALYTICS_ACCESS_TOKEN)
+if [ -z "$BUILDKITE_ANALYTICS_TOKEN" ]; then
+    echo "Error: BUILDKITE_ANALYTICS_TOKEN is not set"
+    exit 1
+fi
+
+
 source ci/buildkite/utils/docker-hub-credentials.sh
 
 # Login to Docker Hub (make sure DOCKER_HUB_USERNAME and DOCKER_HUB_ACCESS_TOKEN are set)
