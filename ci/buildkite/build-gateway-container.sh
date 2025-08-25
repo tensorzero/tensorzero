0b90c69df8f910927b38d73dce9093ffebe2f00e
Viraj Mehta <viraj@tensorzero.com>
2025-08-25 14:34:14 -0400

    push to docker hub

--- ci/buildkite/build-gateway-container.sh
+++ ci/buildkite/build-gateway-container.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
 set -euxo pipefail
 
 # Get the short hash from the buildkite environment variable
 SHORT_HASH=${BUILDKITE_COMMIT:0:7}
-# Build and save container
+
+# Build the container
 docker build --load --build-arg BUILDKIT_CONTEXT_KEEP_GIT_DIR=1 -f gateway/Dockerfile . -t tensorzero/gateway-dev:ci-sha-$SHORT_HASH
-docker save tensorzero/gateway:sha-$SHORT_HASH > gateway-container.tar
+
+# Login to Docker Hub (make sure DOCKER_HUB_USERNAME and DOCKER_HUB_ACCESS_TOKEN are set)
+echo "$DOCKER_HUB_ACCESS_TOKEN" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
+
+# Push to Docker Hub
+docker push tensorzero/gateway-dev:ci-sha-$SHORT_HASH
