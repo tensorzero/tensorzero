6403fd94570e528dcb3f5addd3e2a23b069e0fa8
Gabriel Bianconi <GabrielBianconi@users.noreply.github.com>
2025-09-01 17:40:54 -0400

    Change checksum

--- ci/upload-provider-proxy-cache.sh
+++ ci/upload-provider-proxy-cache.sh
@@ -5,7 +5,8 @@ cd $(dirname $0)/provider-proxy-cache
 export AWS_RETRY_MODE=standard
 export AWS_MAX_ATTEMPTS=10
 
-# Avoid: "Reduce your concurrent request rate for the same object"
+# The following settings attempt to avoid the error:
+# "An error occurred (ServiceUnavailable) when calling the PutObject operation: Reduce your concurrent request rate for the same object."
 aws configure set default.s3.max_concurrent_requests 3
 aws configure set default.s3.multipart_threshold 64MB
 aws configure set default.s3.multipart_chunksize 64MB
@@ -13,4 +14,4 @@ aws configure set default.s3.multipart_chunksize 64MB
 # Use --delete to remove stale cache files from the bucket.
 # This doesn't do anything for merge queue runs (since we start by downloading the entire bucket),
 # but it will help for cron job runs, where we intentionally start with an empty provider-proxy cache.
-aws s3 --endpoint-url https://19918a216783f0ac9e052233569aef60.r2.cloudflarestorage.com/ sync . s3://provider-proxy-cache --checksum-algorithm CRC32 --delete --no-progress
+aws s3 --endpoint-url https://19918a216783f0ac9e052233569aef60.r2.cloudflarestorage.com/ sync . s3://provider-proxy-cache --delete --no-progress
