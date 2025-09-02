00d281b86d5486d5ae745940d605b3ec43cb5b4a
Viraj Mehta <viraj@tensorzero.com>
2025-09-02 11:23:00 -0400

    added a sleep

--- ci/buildkite/test-clickhouse-cloud.sh
+++ ci/buildkite/test-clickhouse-cloud.sh
@@ -95,5 +95,6 @@ export CLICKHOUSE_HOST=$(echo $TENSORZERO_CLICKHOUSE_URL | sed 's|https://[^@]*@
 export CLICKHOUSE_USER="$CLICKHOUSE_USERNAME"
 export CLICKHOUSE_PASSWORD="$CLICKHOUSE_PASSWORD"
 cd ui/fixtures && ./load_fixtures.sh $TENSORZERO_E2E_TESTS_DATABASE && cd ../..
+sleep 600
 
 cargo test-e2e-no-creds -- --skip test_concurrent_clickhouse_migrations
