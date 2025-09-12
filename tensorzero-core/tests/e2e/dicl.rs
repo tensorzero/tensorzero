ce2ec9e1577427c6519a61aff6541542cdb65256
Aaron Hill <aaron@tensorzero.com>
2025-09-12 09:50:15 -0400

    Fix input check

--- tensorzero-core/tests/e2e/dicl.rs
+++ tensorzero-core/tests/e2e/dicl.rs
@@ -1197,7 +1197,7 @@ async fn test_dicl_json_request() {
         "messages": [
             {
                 "role": "user",
-                "content": [{"type": "text", "value": {"country": "Brazil"}}]
+                "content": [{"type": "template", "name" : "user", "arguments": {"country": "Brazil"}}]
             }
         ]
     });
