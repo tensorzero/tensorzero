bc03cefcd18b4e307516e03f1d7223ba9b76e87c
Bret Hudson <bret@brethudson.com>
2025-09-15 13:07:15 -0700

    Added Playwright test for word wrap

---
+++ ui/e2e_tests/word_wrap.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from "@playwright/test";
+
+test("should show the datapoint detail page", async ({ page }) => {
+  await page.goto(
+    "/datasets/foo/datapoint/0196374b-d575-77b3-ac22-91806c67745c",
+  );
+
+  await expect(page.getByText("Input")).toBeVisible();
+
+  const getWordWrapToggle = () => page.getByTitle("Toggle word wrap").first();
+  const getWordWrap = async () =>
+    await page.evaluate(() => localStorage.getItem("word-wrap"));
+
+  // expect default value for word wrap to be true
+  {
+    const button = getWordWrapToggle();
+    expect(await button.getAttribute("aria-pressed")).toBe("true");
+    expect(await getWordWrap()).toEqual("true");
+  }
+
+  // check that it gets updated when button toggle is clicked
+  {
+    const button = getWordWrapToggle();
+    await button.click();
+    expect(await button.getAttribute("aria-pressed")).toBe("false");
+    expect(await getWordWrap()).toEqual("false");
+  }
+
+  // ensure that it is still set to false on page reload...
+  {
+    await page.reload();
+
+    const button = getWordWrapToggle();
+    expect(await button.getAttribute("aria-pressed")).toBe("false");
+    expect(await getWordWrap()).toEqual("false");
+  }
+});
