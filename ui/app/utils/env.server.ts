b99016524d11b3f80b39aba6fdae260129e0c4bf
Bret Hudson <bret@brethudson.com>
2025-09-23 08:17:09 -0700

    Add TOGETHER_BASE_URL to GitHub Actions & dockerfiles

--- ui/app/utils/env.server.ts
+++ ui/app/utils/env.server.ts
@@ -19,6 +19,7 @@ interface Env {
   OPENAI_BASE_URL: string | null;
   FIREWORKS_BASE_URL: string | null;
   FIREWORKS_ACCOUNT_ID: string | null;
+  TOGETHER_BASE_URL: string | null;
 }
 
 let _env: Env;
@@ -59,6 +60,7 @@ export function getEnv(): Env {
     TENSORZERO_UI_DEFAULT_CONFIG,
     TENSORZERO_GATEWAY_URL,
     OPENAI_BASE_URL: process.env.OPENAI_BASE_URL || null,
+    TOGETHER_BASE_URL: process.env.TOGETHER_BASE_URL || null,
     FIREWORKS_BASE_URL: process.env.FIREWORKS_BASE_URL || null,
     TENSORZERO_EVALUATIONS_PATH:
       process.env.TENSORZERO_EVALUATIONS_PATH || "evaluations",
