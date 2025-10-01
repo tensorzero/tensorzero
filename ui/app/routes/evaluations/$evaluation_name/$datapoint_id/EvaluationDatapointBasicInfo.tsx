33c9ef8260c326416582727f0081f075f17f5860
Shuyang Li <shuyang.li.95@gmail.com>
2025-10-01 16:37:44 -0400

    Add datapoint names to dataset and evaluation displays (tables and details)
    Reorder Name to be right of EpisodeId in DatasetRowTable
    
    Add tests

--- ui/app/routes/evaluations/$evaluation_name/$datapoint_id/EvaluationDatapointBasicInfo.tsx
+++ ui/app/routes/evaluations/$evaluation_name/$datapoint_id/EvaluationDatapointBasicInfo.tsx
@@ -13,12 +13,14 @@ interface BasicInfoProps {
   evaluation_name: string;
   evaluation_config: StaticEvaluationConfig;
   dataset_name: string;
+  datapoint_name: string | null;
 }
 
 export default function BasicInfo({
   evaluation_name,
   evaluation_config,
   dataset_name,
+  datapoint_name,
 }: BasicInfoProps) {
   const functionName = evaluation_config.function_name;
   const functionConfig = useFunctionConfig(functionName);
@@ -29,6 +31,16 @@ export default function BasicInfo({
 
   return (
     <BasicInfoLayout>
+      <BasicInfoItem>
+        <BasicInfoItemTitle>Name</BasicInfoItemTitle>
+        <BasicInfoItemContent>
+          {/* TODO: support editing names */}
+          <Chip
+            label={datapoint_name || "-"}
+            font="mono"
+            />
+        </BasicInfoItemContent>
+      </BasicInfoItem>
       <BasicInfoItem>
         <BasicInfoItemTitle>Evaluation</BasicInfoItemTitle>
         <BasicInfoItemContent>
