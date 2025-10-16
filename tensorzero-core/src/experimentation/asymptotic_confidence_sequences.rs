ddb2a749cad6fe8694e1adb68562a43645588387
Alan Mishler <alan@tensorzero.com>
2025-10-16 14:36:23 -0400

    Initial implementation of asymptotic confidence sequences

--- tensorzero-core/src/experimentation/asymptotic_confidence_sequences.rs
+++ tensorzero-core/src/experimentation/asymptotic_confidence_sequences.rs
@@ -1,25 +1,36 @@
 /// Asymptotic confidence sequences for means
-///
-
-pub fn asymp_cs(means: Vec<f64>, variances: Vec<f64>, counts: Vec<u32>, alpha: f64, rho: f64) {
-    fn single_timepoint_margin(variance: f64, count: f64) {
+pub fn asymp_cs(
+    means: Vec<f64>,
+    variances: Vec<f64>,
+    counts: Vec<u32>,
+    alpha: f64,
+    rho: f64,
+) -> (Vec<f64>, Vec<f64>) {
+    // Margin calculated at a single timepoint
+    let single_timepoint_margin = |variance: f64, count: f64| -> f64 {
         let rho2 = rho * rho;
-        ((count as float * variance * rho2 + 1.0) / (count * count * rho2)
-            * ((count * variance * rho2 + 1) / (alpha * alpha)).ln())
+        ((count * variance * rho2 + 1.0) / (count * count * rho2)
+            * ((count * variance * rho2 + 1.0) / (alpha * alpha)).ln())
         .sqrt()
-    }
-    let margins = variances
-        .zip(counts)
-        .map(|(v, c)| single_timepoint_margin(v, c))
-        .collect();
-    let cs_lower = means
-        .zip(margins)
-        .map(|mean, margin| mean - margin)
-        .collect();
-    let cs_upper = means
-        .zip(margins)
-        .map(|mean, margin| mean + margin)
+    };
+    // Compute margins over all timepoints
+    let margins: Vec<f64> = variances
+        .iter()
+        .zip(counts.iter())
+        .map(|(v, c)| single_timepoint_margin(*v, *c as f64))
         .collect();
 
-    return (cs_lower, cs_upper);
+    let cs_lower: Vec<f64> = means
+        .iter()
+        .zip(margins.iter())
+        .map(|(mean, margin)| mean - margin)
+        .collect();
+
+    let cs_upper: Vec<f64> = means
+        .iter()
+        .zip(margins.iter())
+        .map(|(mean, margin)| mean + margin)
+        .collect();
+
+    (cs_lower, cs_upper)
 }
