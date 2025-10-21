0be4bb42528398d718562745917fc58678c8ff3d
Aaron Hill <aaron@tensorzero.com>
2025-10-21 09:49:00 -0400

    Fix another path

--- ci/upload-browserstack-junit.sh
+++ ci/upload-browserstack-junit.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # cd to the script's directory
 cd "$(dirname "$0")"
 
-python3 ./process_flaky_tests.py $BROWSERSTACK_JUNIT_FILE -o ./processed_junit.xml
+python3 ./process_flaky_tests.py ../$BROWSERSTACK_JUNIT_FILE -o ./processed_junit.xml
 
 curl -u "aaronhill_foWj4v:$BROWSERSTACK_KEY" -vvv \
     -X POST \
