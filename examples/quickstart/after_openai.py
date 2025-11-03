4e89cf9dfa0cc01f3fcd2a02422b86bf2a40f016
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2025-11-03 12:05:11 -0500

    Fix quickstart example

--- examples/quickstart/after_openai.py
+++ examples/quickstart/after_openai.py
@@ -1,14 +1,15 @@
 from openai import OpenAI
 
-with OpenAI(base_url="http://localhost:3000/openai/v1") as client:
-    response = client.chat.completions.create(
-        model="tensorzero::function_name::generate_haiku",
-        messages=[
-            {
-                "role": "user",
-                "content": "Write a haiku about artificial intelligence.",
-            }
-        ],
-    )
+oai = OpenAI(base_url="http://localhost:3000/openai/v1")
+
+response = oai.chat.completions.create(
+    model="tensorzero::function_name::generate_haiku",
+    messages=[
+        {
+            "role": "user",
+            "content": "Write a haiku about artificial intelligence.",
+        }
+    ],
+)
 
 print(response)
