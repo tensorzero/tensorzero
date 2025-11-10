1784c0ffa441bd11821249913948abaf865d56f5
Aaron Hill <aaron@tensorzero.com>
2025-11-10 14:11:18 -0500

    Get rid of extra step

--- ci/upload-python-client.sh
+++ ci/upload-python-client.sh
@@ -4,6 +4,4 @@ set -euxo pipefail
 cd "$(dirname "$0")"
 curl -LsSf https://astral.sh/uv/0.6.4/install.sh | sh
 cd ../clients/python
-uv venv
-uv pip sync requirements.txt
 uv run maturin upload -v --repository $1 --non-interactive --skip-existing ../../wheels-*/*
