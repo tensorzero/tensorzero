a6bd5f1d74b57ec98a90c7e3a4ebe5eb176b61ef
Andrew Jesson <andrew@tensorzero.com>
2025-11-17 14:38:12 -0500

    add evaluate_variant function

--- tensorzero-optimizers/src/gepa/mod.rs
+++ tensorzero-optimizers/src/gepa/mod.rs
@@ -28,7 +28,7 @@ mod analyze;
 mod evaluate;
 
 pub use analyze::{analyze_inferences, Analysis, FunctionContext};
-pub use evaluate::create_evaluation_dataset;
+pub use evaluate::{create_evaluation_dataset, evaluate_variant, EvaluateVariantParams, EvaluationResults};
 
 #[async_trait]
 impl Optimizer for GEPAConfig {
