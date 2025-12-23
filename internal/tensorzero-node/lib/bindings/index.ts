202b17d2a1305895d653958a0585c8bf57fcbece
Shuyang Li <shuyang.li.95@gmail.com>
2025-12-23 19:42:33 +0000

    Implement runEvaluationStreaming as SSE from gateway
    Remove FINAL modifiers from evaluation queries for performance
    
    The FINAL modifier forces ClickHouse to deduplicate at query time,
    which is very expensive on ReplacingMergeTree tables. Queries were
    taking 5+ minutes and timing out.
    
    Removed FINAL from:
    - get_evaluation_result_datapoint_id_subquery (used by multiple queries)
    - list_evaluation_runs
    - search_evaluation_runs
    - get_evaluation_run_infos
    - get_evaluation_run_infos_for_datapoint
    - get_evaluation_results (filtered_dp CTE)

--- internal/tensorzero-node/lib/bindings/index.ts
+++ internal/tensorzero-node/lib/bindings/index.ts
@@ -266,6 +266,7 @@ export * from "./ResolvedTomlPathData";
 export * from "./ResolvedTomlPathDirectory";
 export * from "./RetryConfig";
 export * from "./Role";
+export * from "./RunEvaluationRequest";
 export * from "./SGLangProvider";
 export * from "./SchemaData";
 export * from "./SchemaWithMetadata";
