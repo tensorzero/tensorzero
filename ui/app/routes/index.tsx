05d17c7d081466b4c22dfe20c5705ff0be4c3aa9
Simeon Lee <15081451+simeonlee@users.noreply.github.com>
2026-01-13 17:47:54 -0500

    Update icon highlights on home page

--- ui/app/routes/index.tsx
+++ ui/app/routes/index.tsx
@@ -49,9 +49,9 @@ function DirectoryCard({
   return (
     <Link to={source} className="block">
       <Card className="border-border hover:border-border-hover group flex w-full flex-row items-center gap-3 rounded-xl border p-4 hover:shadow-[0_0_0_3px_rgba(0,0,0,0.05)]">
-        <div className="bg-bg-tertiary h-8 w-8 rounded-lg p-2">
+        <div className="bg-bg-tertiary h-8 w-8 rounded-lg p-2 group-hover:bg-orange-50">
           <Icon
-            className="text-fg-secondary group-hover:text-fg-primary transition-colors"
+            className="text-fg-secondary transition-colors group-hover:text-orange-600"
             size={16}
           />
         </div>
