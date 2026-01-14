d5293ac230729b5a56c3776ed918cbf9d1eeaff4
Gabriel Bianconi <GabrielBianconi@users.noreply.github.com>
2026-01-14 20:20:51 +0000

    Stop making git optional

--- tensorzero-core/src/client/mod.rs
+++ tensorzero-core/src/client/mod.rs
@@ -332,7 +332,6 @@ pub enum TensorZeroError {
         source: TensorZeroInternalError,
     },
     RequestTimeout,
-    #[cfg(feature = "git")]
     Git {
         #[source]
         source: git2::Error,
@@ -355,7 +354,6 @@ impl Display for TensorZeroError {
             }
             TensorZeroError::Other { source } => write!(f, "{source}"),
             TensorZeroError::RequestTimeout => write!(f, "HTTP Error: request timed out"),
-            #[cfg(feature = "git")]
             TensorZeroError::Git { source } => write!(f, "Failed to get git info: {source}"),
         }
     }
