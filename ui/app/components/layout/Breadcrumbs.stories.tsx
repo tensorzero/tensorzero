a5c83e122bc72a0a8eb44bf206aabd9fbcb76d84
Simeon Lee <15081451+simeonlee@users.noreply.github.com>
2026-01-20 10:13:25 -0500

    Remove duplicate withRouter decorator from Breadcrumbs story

--- ui/app/components/layout/Breadcrumbs.stories.tsx
+++ ui/app/components/layout/Breadcrumbs.stories.tsx
@@ -1,12 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { withRouter } from "storybook-addon-remix-react-router";
 import { Breadcrumbs } from "./Breadcrumbs";
 
 const meta = {
   title: "Layout/Breadcrumbs",
   component: Breadcrumbs,
   decorators: [
-    withRouter,
     (Story) => (
       <div className="p-4">
         <Story />
