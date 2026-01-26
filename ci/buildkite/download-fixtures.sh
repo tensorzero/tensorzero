4f661ad7b86ae83d347d69de2bcd81f768e26550
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2026-01-26 17:57:42 -0500

    Fix

--- ci/buildkite/download-fixtures.sh
+++ ci/buildkite/download-fixtures.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -euxo pipefail
 
+export R2_ACCESS_KEY_ID=$(buildkite-agent secret get R2_ACCESS_KEY_ID)
+if [ -z "$R2_ACCESS_KEY_ID" ]; then
+    echo "Error: R2_ACCESS_KEY_ID is not set"
+    exit 1
+fi
+
+export R2_SECRET_ACCESS_KEY=$(buildkite-agent secret get R2_SECRET_ACCESS_KEY)
+if [ -z "$R2_SECRET_ACCESS_KEY" ]; then
+    echo "Error: R2_SECRET_ACCESS_KEY is not set"
+    exit 1
+fi
+
 # Install `uv`
 curl -LsSf https://astral.sh/uv/0.6.17/install.sh | sh
 source $HOME/.local/bin/env
