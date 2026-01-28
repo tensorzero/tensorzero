7edb24480ec42bc6b1358a1709970b15ecc916f6
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2026-01-28 13:22:33 -0500

    Upgrade Python version management

--- ci/upload-python-client.sh
+++ ci/upload-python-client.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 cd "$(dirname "$0")"
-curl -LsSf https://astral.sh/uv/0.6.4/install.sh | sh
+curl -LsSf https://astral.sh/uv/0.9.27/install.sh | sh
 cd ../clients/python
 uv sync
 uv run maturin upload -v --repository $1 --non-interactive --skip-existing ../../wheels-*/*
