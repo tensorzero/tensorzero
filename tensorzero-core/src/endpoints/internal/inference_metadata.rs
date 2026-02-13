5dc1200c97115eab58d4eb0518e9de951e368afb
Shuyang Li <shuyang.li.95@gmail.com>
2026-02-12 21:37:17 -0500

    Make list_inference_metadata endpoint call postgres when enabled

--- tensorzero-core/src/endpoints/internal/inference_metadata.rs
+++ tensorzero-core/src/endpoints/internal/inference_metadata.rs
@@ -6,6 +6,7 @@ use serde::{Deserialize, Serialize};
 use tracing::instrument;
 use uuid::Uuid;
 
+use crate::db::delegating_connection::DelegatingDatabaseConnection;
 use crate::db::inferences::{
     DEFAULT_INFERENCE_QUERY_LIMIT, InferenceMetadata, InferenceQueries,
     ListInferenceMetadataParams, PaginationParams,
@@ -64,10 +65,11 @@ pub async fn get_inference_metadata_handler(
         episode_id: params.episode_id,
     };
 
-    let inference_metadata = app_state
-        .clickhouse_connection_info
-        .list_inference_metadata(&list_params)
-        .await?;
+    let database = DelegatingDatabaseConnection::new(
+        app_state.clickhouse_connection_info.clone(),
+        app_state.postgres_connection_info.clone(),
+    );
+    let inference_metadata = database.list_inference_metadata(&list_params).await?;
 
     Ok(Json(ListInferenceMetadataResponse { inference_metadata }))
 }
