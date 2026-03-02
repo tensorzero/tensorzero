f1a1494195be36bcbdb7005905658a02d0e4186b
Andrew Jesson <andrew@tensorzero.com>
2026-03-02 10:11:33 -0500

    Add missing ts-bindings type name methods

--- internal/autopilot-tools/src/tools/prod/get_feedback_by_target_id.rs
+++ internal/autopilot-tools/src/tools/prod/get_feedback_by_target_id.rs
@@ -43,8 +43,18 @@ impl ToolMetadata for GetFeedbackByTargetIdTool {
     }
 
     #[cfg(feature = "ts-bindings")]
+    fn llm_params_ts_bundle_type_name() -> String {
+        "GetFeedbackByTargetIdToolParams".to_string()
+    }
+
+    #[cfg(feature = "ts-bindings")]
     fn output_ts_bundle() -> tensorzero_ts_types::TsTypeBundle {
         tensorzero_ts_types::GET_FEEDBACK_BY_TARGET_ID_RESPONSE
+    }
+
+    #[cfg(feature = "ts-bindings")]
+    fn output_ts_bundle_type_name() -> String {
+        "GetFeedbackByTargetIdResponse".to_string()
     }
 
     fn name(&self) -> Cow<'static, str> {
