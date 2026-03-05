fdab054f5b5c1bf3c0cb19241e5d74c50c2fd529
Aaron Hill <aaron@tensorzero.com>
2026-03-05 17:02:42 -0500

    Run fmt

--- internal/autopilot-client/src/client.rs
+++ internal/autopilot-client/src/client.rs
@@ -329,7 +329,10 @@ impl AutopilotClient {
                 true
             }
             EventPayload::ToolCall(tool_call) if !available_tools.contains(&tool_call.name) => {
-                tracing::warn!("Ignoring autopilot tool call of unknown tool `{}`", tool_call.name);
+                tracing::warn!(
+                    "Ignoring autopilot tool call of unknown tool `{}`",
+                    tool_call.name
+                );
                 true
             }
             _ => false,
