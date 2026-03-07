de0199f3f948053d6bb5fa6341113123af65f92b
Gabriel Bianconi <1275491+GabrielBianconi@users.noreply.github.com>
2026-03-06 21:59:48 -0500

    testing do not merge

--- ui/app/root.tsx
+++ ui/app/root.tsx
@@ -9,6 +9,8 @@ import {
   useRouteLoaderData,
 } from "react-router";
 
+// ?
+
 import { EMPTY_CONFIG } from "./context/config";
 import type { UiConfig } from "./types/tensorzero";
 import type { Route } from "./+types/root";
