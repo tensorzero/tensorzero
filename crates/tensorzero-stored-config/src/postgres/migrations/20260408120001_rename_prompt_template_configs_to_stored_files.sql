0fc78708f3ba3fd1e204a33393b087a0bea2a144
Aaron Hill <aaron@tensorzero.com>
2026-04-08 14:56:13 -0400

    Combine migrations

--- crates/tensorzero-stored-config/src/postgres/migrations/20260408120001_rename_prompt_template_configs_to_stored_files.sql
+++ crates/tensorzero-stored-config/src/postgres/migrations/20260408120001_rename_prompt_template_configs_to_stored_files.sql
@@ -2,3 +2,5 @@ ALTER TABLE tensorzero.prompt_template_configs RENAME TO stored_files;
 
 ALTER INDEX tensorzero.idx_prompt_template_configs_content_lookup
     RENAME TO idx_stored_files_content_lookup;
+
+ALTER TABLE tensorzero.stored_files RENAME COLUMN template_key TO file_path;
--- crates/tensorzero-stored-config/src/postgres/migrations/20260408120002_rename_template_key_to_file_path.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE tensorzero.stored_files RENAME COLUMN template_key TO file_path;
